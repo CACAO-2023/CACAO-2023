98aa505aca4c69a2cb9c58b29d94e1e24b9ae8c1
Gabriel <Gabriel@172.16.135.42>
2023-03-19 18:14:23 +0100

    Récolte V1

--- src/abstraction/eq3Producteur3/Producteur3.java
+++ src/abstraction/eq3Producteur3/Producteur3.java
@@ -1,8 +1,84 @@
 package abstraction.eq3Producteur3;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Set;
+
+import abstraction.eqXRomu.produits.Lot;
+
 public class Producteur3 extends Producteur3Acteur  {
+	/*
+	 * ChampsH est un dictionnaire qui contient les champs Haut de Gamme
+	 * On associe à un ensemble d'hectars un int qui correspond à leur step de plantaison 
+	 *
+	 *private HashMap<Integer,String> ChampsH;//UTILE ?
+	 *
+	 * ChampsM est un dictionnaire qui contient les champs Moyen de Gamme
+	 * On associe à un ensemble d'hectars un int qui correspond à leur step de plantaison 
+	 *
+	 *private HashMap<Integer,String> ChampsM;//UTILE ?
+	 *
+	 * On crée un dictionnaire qui associe à la clef H ou M le dico ChampsM ou ChapmsH
+	 */
+	private HashMap<String,HashMap> Champs;
 	
+	/*
+	 * Je n'ai pas trouve le type du champs donc j'ai choisit String. A CHANGER
+	 * Il faudra aussi penser à se mettre d'accord sur les tailles des champs initiaux.
+	 */
 	public Producteur3() {
 		super();
+		HashMap<Integer,String> ChampsInitialeH = new HashMap<Integer,String>();
+		HashMap<Integer,String> ChampsInitialeM = new HashMap<Integer,String>();
+		HashMap<String,HashMap> ChampsInitiale = new HashMap<String,HashMap>();
+		ChampsInitiale.put( "H",ChampsInitialeH);
+		ChampsInitiale.put( "M",ChampsInitialeM);
+		this.Champs=ChampsInitiale;	
+	}
+	public Producteur3(HashMap<String,HashMap> m) {
+		this.Champs=m;
+	}
+	
+	public LinkedList<Lot> Harvest(int CurrentStep, HashMap<String,HashMap> Fields) {
+		if(Champs==null) {
+			throw new IllegalArgumentException("HashMap<String,HashMap> Fields ==null --> Pb");
+		}
+		/*
+		 * Récolte des fèves moyennes gammes
+		 */
+		HashMap<Integer,String> FieldM = Fields.get("M");
+		//On récupère la liste des clefs de FieldM
+		Set<Integer> KeysM = FieldM.keySet();
+		//On crée une liste qui contient les clefs de tous les champs M qui doivent être récoltés
+		LinkedList<Integer> HarvestKeysM = new LinkedList<Integer>();
+		//On regarde si un des champs de FieldM doit être récolté sachant qu'un champ M doit être récolté tous les 6 mois
+		for(Integer key : KeysM) {
+			/*DateActuelle-DatePlantaison=DureeGestation. Si DureeGestation est divisible par 12(nombre de semaines
+			*dans 6 mois), alors le champ M doit être récolté
+			**/
+			if((CurrentStep - key)%12==0) {
+				HarvestKeysM.add(key);
+			}
+		}
+		//Lot lotH = HarvestM(HarvestKeysM,FieldM);
+		/*
+		 * Récolte des fèves hautes gammes
+		 */
+		HashMap<Integer,String> FieldH = Fields.get("H");
+		//On récupère la liste des clefs de FieldH
+		Set<Integer> KeysH = FieldH.keySet();
+		//On crée une liste qui contient les clefs de tous les champs H qui doivent être récoltés
+		LinkedList<Integer> HarvestKeysH = new LinkedList<Integer>();
+		//On regarde si un des champs de FieldH doit être récolté sachant qu'un champs H doit être récolté tous les 7 mois
+		for(Integer key : KeysM) {
+			/*DateActuelle-DatePlantaison=DureeGestation. Si DureeGestation est divisible par 14(nombre de semaines
+			*dans 7 mois), alors le champ H doit être récolté
+			**/
+			if((CurrentStep - key)%14==0) {
+				HarvestKeysH.add(key);
+			}
+		}
+		//PARTIE RÉCOLTE-->Lot
+		return null;
 	}
 }
