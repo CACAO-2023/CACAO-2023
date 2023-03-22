e5adfbdaa939a9b974b45a353d027eac76b8c2f6
addda <addda@172.16.128.159>
2023-03-22 10:28:08 +0100

    Ajout de la liste des chocolats dans la classe transformateur

--- src/abstraction/eq5Transformateur2/Transformateur2.java
+++ src/abstraction/eq5Transformateur2/Transformateur2.java
@@ -1,8 +1,25 @@
 package abstraction.eq5Transformateur2;
 
+import java.util.List;
+
+import abstraction.eqXRomu.produits.Chocolat;
+import abstraction.eqXRomu.produits.ChocolatDeMarque;
+
 public class Transformateur2 extends Transformateur2Acteur  {
 	
+	private List<ChocolatDeMarque>chocosProduits;
+
 	public Transformateur2() {
 		super();
+	}
+	
+	public List<ChocolatDeMarque> getChocolatsProduits() {
+		if (this.chocosProduits.size()==0) {
+				Chocolat c1 = Chocolat.C_MQ;
+				Chocolat c2 = Chocolat.C_HQ_BE;
+				this.chocosProduits.add(new ChocolatDeMarque(c1, "ChocoPop", 70, 0));
+				this.chocosProduits.add(new ChocolatDeMarque(c2, "Maison Doutre", 90, 10));
+		}
+		return this.chocosProduits;
 	}
 }
