71e65958077e601abc1b3b1bbe36de850b336078
eloua <eloua@172.16.127.131>
2023-03-22 08:46:19 +0100

    elouan classe champ et hectar

--- src/abstraction/eq1Producteur1/champ.java
+++ src/abstraction/eq1Producteur1/champ.java
@@ -1,5 +1,18 @@
 package abstraction.eq1Producteur1;
 
+import java.util.LinkedList;
+
+
+//classe écrite par Elouan
+
 public class champ {
+	private LinkedList<hectar> hectares;
+	
+	public LinkedList<hectar> getHectares(){
+		return this.hectares;
+	}
+	public void add(hectar h) {
+		this.getHectares().add(h);
+	}
 
 }
