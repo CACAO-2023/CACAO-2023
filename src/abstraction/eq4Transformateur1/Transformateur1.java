ec7c9c372cac1142b7122868b4b71399f9843268
Fouad <Fouad@LAPTOP-5G3O89GR>
2023-03-22 09:04:48 +0100

    Transfo eq4

--- src/abstraction/eq4Transformateur1/Transformateur1.java
+++ src/abstraction/eq4Transformateur1/Transformateur1.java
@@ -1,8 +1,19 @@
 package abstraction.eq4Transformateur1;
 
-public class Transformateur1 extends Transformateur1Acteur  {
+import java.util.List;
+
+import abstraction.eqXRomu.filiere.IFabricantChocolatDeMarque;
+import abstraction.eqXRomu.produits.ChocolatDeMarque;
+
+public class Transformateur1 extends Transformateur1Acteur implements IFabricantChocolatDeMarque  {
 	
 	public Transformateur1() {
 		super();
+	}
+
+	@Override
+	public List<ChocolatDeMarque> getChocolatsProduits() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
