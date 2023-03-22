4e7780187f91084e7aaea98c82de7c3fdbe98590
charlespelong <charlespelong@dhcp-124-126.mde.emn.fr>
2023-03-22 11:54:14 +0100

    Merge branch 'main' of https://github.com/Charles141002/CACAO-2023 into main

--- src/abstraction/eq1Producteur1/stockFeve.java
+++ src/abstraction/eq1Producteur1/stockFeve.java
@@ -5,21 +5,21 @@ import java.util.LinkedList;
 //elouan
  
 public class stockFeve {
-	private LinkedList<Feve> stock;
+	private LinkedList<Feve1> stock;
 
-	public LinkedList<Feve> getStock(){
+	public LinkedList<Feve1> getStock(){
 		return this.stock;
 	}
-	public Feve getFeve(int i) {
+	public Feve1 getFeve(int i) {
 		return this.getStock().get(i);
 	}
-	public void ajouterFeve(Feve f) {
+	public void ajouterFeve(Feve1 f) {
 		this.getStock().add(f);
 	}
 	public int nbFeve() {
 		return this.getStock().size();
 	}
-	public void suppFeve(Feve f) {
+	public void suppFeve(Feve1 f) {
 		this.getStock().remove(f);
 	}
 	public void suppFeve(int i) {
