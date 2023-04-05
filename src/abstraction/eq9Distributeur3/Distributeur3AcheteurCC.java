eca98b548d7faa96ed70d051cbd0d18a69023a84
samia <samia@172.16.131.248>
2023-04-05 09:19:34 +0200

    Merge branch 'main' of https://github.com/BaptisteBAYLE/CACAO-2023 into main

--- src/abstraction/eq9Distributeur3/Distributeur3AcheteurCC.java
+++ src/abstraction/eq9Distributeur3/Distributeur3AcheteurCC.java
@@ -1,27 +1,33 @@
 package abstraction.eq9Distributeur3;
 
+import java.awt.Color;
+
 import abstraction.eqXRomu.contratsCadres.Echeancier;
 import abstraction.eqXRomu.contratsCadres.ExemplaireContratCadre;
 import abstraction.eqXRomu.contratsCadres.IAcheteurContratCadre;
 import abstraction.eqXRomu.contratsCadres.IVendeurContratCadre;
+import abstraction.eqXRomu.general.Journal;
 import abstraction.eqXRomu.produits.IProduit;
 import abstraction.eqXRomu.produits.Lot;
 
 public class Distributeur3AcheteurCC extends Distributeur3Acteur implements IAcheteurContratCadre{
+	public static Color COLOR_LLGRAY = new Color(238,238,238);
+	protected Integer cryptogramme;
+	protected Journal journal;
 	
 	
 	
 	public void next() {
+		super.next();
 		
 	}
 
-	@Override
 	public boolean achete(IProduit produit) {
-		// TODO Auto-generated method stub
-		return false;
+		this.
+		journal.ajouter(COLOR_LLGRAY, Color.BLUE, "  CCA : j'affirme vouloir acheter le produit "+produit);
+		return true;
 	}
 
-	@Override
 	public int fixerPourcentageRSE(IAcheteurContratCadre acheteur, IVendeurContratCadre vendeur, IProduit produit,
 			Echeancier echeancier, long cryptogramme, boolean tg) {
 		// TODO Auto-generated method stub
