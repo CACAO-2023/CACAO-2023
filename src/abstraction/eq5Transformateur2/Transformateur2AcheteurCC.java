82f41c342153419767b3a934a1a50352945aa8a7
wlabb <wlabb@172.17.3.156>
2023-04-05 10:39:15 +0200

    cc acheteur

--- src/abstraction/eq5Transformateur2/Transformateur2AcheteurCC.java
+++ src/abstraction/eq5Transformateur2/Transformateur2AcheteurCC.java
@@ -1,16 +1,20 @@
 package abstraction.eq5Transformateur2;
 
+import java.awt.Color;
+
 import abstraction.eqXRomu.contratsCadres.Echeancier;
 import abstraction.eqXRomu.contratsCadres.ExemplaireContratCadre;
 import abstraction.eqXRomu.contratsCadres.IAcheteurContratCadre;
 import abstraction.eqXRomu.contratsCadres.IVendeurContratCadre;
 import abstraction.eqXRomu.contratsCadres.SuperviseurVentesContratCadre;
 import abstraction.eqXRomu.filiere.Filiere;
+import abstraction.eqXRomu.produits.ChocolatDeMarque;
 import abstraction.eqXRomu.produits.IProduit;
 import abstraction.eqXRomu.produits.Lot;
 
 public class Transformateur2AcheteurCC extends Transformateur2Acteur implements IAcheteurContratCadre {
 
+	public static Color COLOR_LLGRAY = new Color(238,238,238);
 	protected SuperviseurVentesContratCadre superviseurVentesCC;
 	public void initialiser() {
 		super.initialiser();
@@ -20,21 +24,38 @@ public class Transformateur2AcheteurCC extends Transformateur2Acteur implements 
 	@Override
 	public boolean achete(IProduit produit) {
 		// TODO Auto-generated method stub
-		return false;
+		if (produit instanceof ChocolatDeMarque) {
+			this.journal.ajouter(COLOR_LLGRAY, Color.BLUE, "  CCA : j'affirme vouloir acheter le produit "+produit);
+		return true;}
+		else {
+			this.journal.ajouter(COLOR_LLGRAY, Color.BLUE, "  CCA : j'affirme ne pas vouloir acheter le produit "+produit);
+		return false;}
+		
+		
+		
 	}
 
 	@Override
 	public int fixerPourcentageRSE(IAcheteurContratCadre acheteur, IVendeurContratCadre vendeur, IProduit produit,
 			Echeancier echeancier, long cryptogramme, boolean tg) {
-		// TODO Auto-generated method stub
-		return 0;
+		if ((( ((ChocolatDeMarque) produit).getMarque())) == "Maison Doutre") {
+			return 10; }
+		else { 
+			return 0; 
+			}
 	}
+		// TODO Auto-generated method stub
+
 
 	@Override
 	public Echeancier contrePropositionDeLAcheteur(ExemplaireContratCadre contrat) {
-		// TODO Auto-generated method stub
-		return null;
+		if (contrat.getTeteGondole()) {
+			return null;
+		}
+		this.journal.ajouter(COLOR_LLGRAY, Color.BLUE, "  CCA : j'accepte l'echeancier "+contrat.getEcheancier());
+		return contrat.getEcheancier();
 	}
+		
 
 	@Override
 	public double contrePropositionPrixAcheteur(ExemplaireContratCadre contrat) {
