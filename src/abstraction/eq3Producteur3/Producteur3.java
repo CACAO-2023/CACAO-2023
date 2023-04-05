557c2b1478daab4c041f8695152b1477b8f0f8d9
VictorDCh <vdubuschanson@outlook.fr>
2023-04-05 09:07:58 +0200

    Changed method variationBesoinHectares()

--- src/abstraction/eq3Producteur3/Producteur3.java
+++ src/abstraction/eq3Producteur3/Producteur3.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Set;
 
+import abstraction.eqXRomu.produits.Feve;
 import abstraction.eqXRomu.produits.Lot;
 
 public class Producteur3 extends Producteur3Acteur  {
@@ -27,7 +28,7 @@ public class Producteur3 extends Producteur3Acteur  {
 	private Integer HectaresLibres; /*Repertorie le nombre d'hectares que l'on possede*/
 	private Integer HectaresUtilises; /*Repertorie le nombre d'hectares que l'on utilise*/
 	private Integer CoutStep; /* Tout nos couts du step, reinitialises a zero au debut de chaque step et payes a la fin du step*/
-	private Producteur3Stock Stock;
+	private Stock Stock;
 	/*
 	 * Je n'ai pas trouve le type du champs donc j'ai choisit String. A CHANGER
 	 * Il faudra aussi penser a se mettre d'accord sur les tailles des champs initiaux.
@@ -35,7 +36,7 @@ public class Producteur3 extends Producteur3Acteur  {
 	public Producteur3() {
 		super();
 		this.fields = new Champs();
-		this.Stock = new Producteur3Stock();
+		this.Stock = new Stock();
 		Integer HectaresLibres = 0;
 		Integer HectaresUtilises = 950000;
 		Integer CoutStep = 0;
@@ -46,14 +47,13 @@ public class Producteur3 extends Producteur3Acteur  {
 	public Champs getFields() {
 		return this.fields;
 	}
-	private Producteur3Stock getStock() {
+	private Stock getStock() {
 		// TODO Auto-generated method stub
 		return this.Stock;
 	}
   
 	
-	/*
-
+	/**
 	 * @author Dubus-Chanson Victor
 	 */
 	public void addCoutHectaresUtilises() {
@@ -69,7 +69,7 @@ public class Producteur3 extends Producteur3Acteur  {
 	
 	public void HarvestToStock(int step) {
 		LinkedList<Integer> quantite = this.getFields().HarvestHM(step);
-		Producteur3Stock Stock = this.getStock();
+		Stock Stock = this.getStock();
 		if(quantite.get(0) > 0) {
 		Stock.ajouterH(step, quantite.get(0));
 		}
@@ -79,12 +79,24 @@ public class Producteur3 extends Producteur3Acteur  {
 	}
 
 
-
+	/**
+	 * @author Dubus-Chanson Victor
+	 */
+	
 	/*Calcule le nombre d'Hectares (uniquement positif ou nul) que l'on a besoin de rajouter a la partie cultivee (seulement tous les 6 mois)*/
 	/*A modifier, a besoin des quantites de feves echangees (via stock)*/
 	public Integer variationBesoinHectares() {
-		Integer NbHectares = 0;
-		return NbHectares;
+		Integer Besoin = 0;
+		Stock Stock = this.getStock();
+		Double Quantite_HQ_BE= Stock.getQuantite(Feve.F_HQ_BE);
+		Double Quantite_MQ_BE= Stock.getQuantite(Feve.F_MQ_BE);
+		if (Quantite_HQ_BE < 100) {
+			Besoin += 100; /*56 tonnes de plus par an à partir de 5ans*/
+		}
+		if (Quantite_MQ_BE < 100) {
+			Besoin += 100; /*56 tonnes de plus par an à partir de 5ans*/
+		}
+		return Besoin;
 	}
 	
 	public void achatHectares(Integer HectaresAAcheter) {
@@ -96,12 +108,11 @@ public class Producteur3 extends Producteur3Acteur  {
 	public void changeHectaresAndCoutsLies(Integer ajoutHectares, Integer HectaresLiberes) {
 		this.HectaresUtilises = this.HectaresUtilises + ajoutHectares;
 		this.HectaresLibres = this.HectaresLibres + HectaresLiberes;
-		Integer HectaresAAcheter = this.HectaresLibres - ajoutHectares;
+		Integer HectaresAAcheter = ajoutHectares - this.HectaresLibres;
 		if (HectaresAAcheter > 0) {
 			this.achatHectares(HectaresAAcheter);
 		}
-		this.HectaresUtilises = this.HectaresUtilises + HectaresAAcheter;
-		this.HectaresLibres = this.HectaresLibres - ajoutHectares;
+		this.HectaresLibres = ajoutHectares - this.HectaresLibres;
 		if (this.HectaresLibres < 0) {
 			this.HectaresLibres = 0;
 		}
