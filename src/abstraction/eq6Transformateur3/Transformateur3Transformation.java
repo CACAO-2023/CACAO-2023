0b4f61023a4aaae180b25ebecaf78dc1c65b913a
bedum <bedum@172.16.128.126>
2023-04-05 10:18:57 +0200

    Transformation (actualisation des stocks sans contrainte de temps)

--- src/abstraction/eq6Transformateur3/Transformateur3Transformation.java
+++ src/abstraction/eq6Transformateur3/Transformateur3Transformation.java
@@ -20,12 +20,34 @@ public class Transformateur3Transformation extends Transformateur3Stocks {
 	           
 	*/
 	
-	public void transformationChoco(Feve f) {
+	public void transformationChoco(Feve f, double qte) {
 		if (f instanceof F_BQ) {
-			Variable pourcentageTransfo = this.getPourcentageCacaoBG();
-			stockFeve.replace(F_BQ, )
-		}
-		
-	}
+			double pourcentageTransfo = this.getPourcentageCacaoBG().getValeur();
+			double a=stockFeve.get(F_BQ);
+			stockFeve.replace(F_BQ, a-(pourcentageTransfo*qte));
+			double b=stockChocolat.get(C_BQ);
+			stockChocolat.replace(C_BQ,b+qte);
+			} else {
+				if (f instanceof F_MQ) {
+					double pourcentageTransfo = this.getPourcentageCacaoBG().getValeur();
+					double a=stockFeve.get(F_MQ);
+					stockFeve.replace(F_MQ, a-(pourcentageTransfo*qte));
+					double b=stockChocolat.get(C_MQ);
+					stockChocolat.replace(C_MQ,b+qte);
+					} else {
+						if (f instanceof F_MQ_BE) {
+							double pourcentageTransfo = this.getPourcentageCacaoBG().getValeur();
+							double a=stockFeve.get(F_MQ_BE);
+							stockFeve.replace(F_MQ_BE, a-(pourcentageTransfo*qte));
+							double b=stockChocolat.get(C_MQ_BE);
+							stockChocolat.replace(C_MQ_BE,b+qte);
+							} else {
+								if (f instanceof F_HQ_BE) {
+									double pourcentageTransfo = this.getPourcentageCacaoBG().getValeur();
+									double a=stockFeve.get(F_HQ_BE);
+									stockFeve.replace(F_HQ_BE, a-(pourcentageTransfo*qte));
+									double b=stockChocolat.get(C_HQ_BE);
+									stockChocolat.replace(C_HQ_BE,b+qte);
+							}
 	
 }
