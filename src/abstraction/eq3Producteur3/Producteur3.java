6462e2e249099aa545782ea3e62d49e9ec41d3a2
Gabriel <Gabriel@172.17.2.18>
2023-04-05 08:07:22 +0200

    zzuiojuhgtf

--- src/abstraction/eq3Producteur3/Producteur3.java
+++ src/abstraction/eq3Producteur3/Producteur3.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Set;
 
+import abstraction.eqXRomu.produits.Lot;
+
 public class Producteur3 extends Producteur3Acteur  {
 	/*
 	 * ChampsH est un dictionnaire qui contient les champs Haut de Gamme
@@ -47,55 +49,11 @@ public class Producteur3 extends Producteur3Acteur  {
 		// TODO Auto-generated method stub
 		return this.Stock;
 	}
-	/**
   
 	public Producteur3(HashMap<String,HashMap> m) {
 		this.Champs=m;
 	}
 	
-	public LinkedList<Lot> Harvest(int CurrentStep, HashMap<String,HashMap> Fields) {
-		if(Champs==null) {
-			throw new IllegalArgumentException("HashMap<String,HashMap> Fields ==null --> Pb");
-		}
-		/*
-		 * Recolte des feves moyennes gammes
-		 */
-		HashMap<Integer,String> FieldM = Fields.get("M");
-		//On recupere la liste des clefs de FieldM
-		Set<Integer> KeysM = FieldM.keySet();
-		//On cree une liste qui contient les clefs de tous les champs M qui doivent etre recoltes
-		LinkedList<Integer> HarvestKeysM = new LinkedList<Integer>();
-		//On regarde si un des champs de FieldM doit etre recolte sachant qu'un champ M doit etre recolte tous les 6 mois
-		for(Integer key : KeysM) {
-			/*DateActuelle-DatePlantaison=DureeGestation. Si DureeGestation est divisible par 12(nombre de semaines
-			*dans 6 mois), alors le champ M doit tre recolte
-			**/
-			if((CurrentStep - key)%12==0) {
-				HarvestKeysM.add(key);
-			}
-		}
-		//Lot lotH = HarvestM(HarvestKeysM,FieldM);
-		/*
-		 * Recolte des feves hautes gammes
-		 */
-		HashMap<Integer,String> FieldH = Fields.get("H");
-		//On recupere la liste des clefs de FieldH
-		Set<Integer> KeysH = FieldH.keySet();
-		//On cree une liste qui contient les clefs de tous les champs H qui doivent etre recoltes
-		LinkedList<Integer> HarvestKeysH = new LinkedList<Integer>();
-		//On regarde si un des champs de FieldH doit etre recolte sachant qu'un champs H doit etre recolte tous les 7 mois
-		for(Integer key : KeysM) {
-			/*DateActuelle-DatePlantaison=DureeGestation. Si DureeGestation est divisible par 14(nombre de semaines
-			*dans 7 mois), alors le champ H doit etre recolte
-			**/
-			if((CurrentStep - key)%14==0) {
-				HarvestKeysH.add(key);
-			}
-		}
-		//PARTIE RCOLTE-->Lot
-		return null;
-	}
-  
 	/*
 
 	 * @author Dubus-Chanson Victor
