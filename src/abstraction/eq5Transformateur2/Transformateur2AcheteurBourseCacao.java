c4937c7df040df94d9556d892ec3bb03ddb0e46f
Mathis Doutre <mathis.doutre@gmail.com>
2023-04-05 08:40:14 +0200

    Constructeur et suppression des getters et setters inutiles

--- src/abstraction/eq5Transformateur2/Transformateur2AcheteurBourseCacao.java
+++ src/abstraction/eq5Transformateur2/Transformateur2AcheteurBourseCacao.java
@@ -3,22 +3,33 @@ package abstraction.eq5Transformateur2;
 import abstraction.eqXRomu.bourseCacao.BourseCacao;
 import abstraction.eqXRomu.bourseCacao.IAcheteurBourse;
 import abstraction.eqXRomu.filiere.Filiere;
+import abstraction.eqXRomu.general.Variable;
 import abstraction.eqXRomu.produits.Feve;
 import abstraction.eqXRomu.produits.Lot;
 
 public class Transformateur2AcheteurBourseCacao extends Transformateur2 implements IAcheteurBourse {
 
 	private double achatMaxParStep;
+	private Feve feve;
+	private Variable stockFeve;
 	
-	public Transformateur2AcheteurBourseCacao() {
+	public Transformateur2AcheteurBourseCacao(Feve f, Variable s, double a) {
 		super();
+		this.achatMaxParStep = a;
+		this.feve = f;
+		this.stockFeve = s;
 	}
 	
+	public Feve getFeve() {
+		return feve;
+	}
+
+
 	@Override
 	public double demande(Feve f, double cours) {
 		if (this.getFeve().equals(f)) {
 			BourseCacao bourse = (BourseCacao)(Filiere.LA_FILIERE.getActeur("BourseCacao"));
-			double pourcentage = (bourse.getCours(getFeve()).getMax()-bourse.getCours(getFeve()).getValeur())/(bourse.getCours(getFeve()).getMax()-bourse.getCours(getFeve()).getMin());
+			double pourcentage = (bourse.getCours(getFeve()).getMax()-bourse.getCours(getFeve()).getValeur())/(bourse.getCours(getFeve()).getMax()-bourse.getCours(getFeve()).getMin()); // difference de prix avec le max / amplitude totale
 			return achatMaxParStep*pourcentage;
 		} else {
 			return 0.0;
