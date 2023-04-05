6834d8f2eafb78d99bf279b7bc140da85796d86b
addda <addda@172.17.3.10>
2023-04-05 10:33:16 +0200

    Ajout du step courant et des stocks en début

--- src/abstraction/eq5Transformateur2/Transformateur2.java
+++ src/abstraction/eq5Transformateur2/Transformateur2.java
@@ -3,6 +3,7 @@ package abstraction.eq5Transformateur2;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.awt.Color;
 
 import abstraction.eqXRomu.filiere.Filiere;
 import abstraction.eqXRomu.filiere.IFabricantChocolatDeMarque;
@@ -60,7 +61,16 @@ public class Transformateur2 extends Transformateur2Acteur implements IFabricant
 	
 	public void next() {
 		super.next();
-		this.journal.ajouter("Step numéro "+ Filiere.LA_FILIERE.getEtape());
+		this.journal.ajouter("=== Step numéro "+ Filiere.LA_FILIERE.getEtape()+" ===");
+		
 		this.journal.ajouter("=== STOCKS === ");
+		for (Feve f : Feve.values()) {
+			this.journal.ajouter("Stock de "+Journal.texteSurUneLargeurDe(f+"", 15)+" = "+this.stockFeves.get(f));
+		}
+		for (Chocolat c : Chocolat.values()) {
+			this.journal.ajouter("Stock de "+Journal.texteSurUneLargeurDe(c+"", 15)+" = "+this.stockChoco.get(c));
+		}
+		
+		
 		}
 }
