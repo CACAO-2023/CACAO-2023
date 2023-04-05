a3df0512109ab75bbe5cec577a132519382d395e
addda <addda@172.17.3.10>
2023-04-05 08:29:53 +0200

    Fonction initialiser transformateur

--- src/abstraction/eq5Transformateur2/Transformateur2.java
+++ src/abstraction/eq5Transformateur2/Transformateur2.java
@@ -1,18 +1,52 @@
 package abstraction.eq5Transformateur2;
 
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+
+import abstraction.eqXRomu.filiere.Filiere;
 import abstraction.eqXRomu.filiere.IFabricantChocolatDeMarque;
 import abstraction.eqXRomu.produits.Chocolat;
 import abstraction.eqXRomu.produits.ChocolatDeMarque;
+import abstraction.eqXRomu.produits.Feve;
 
-public class Transformateur2 extends Transformateur2AcheteurCC implements IFabricantChocolatDeMarque  {
+public class Transformateur2 extends Transformateur2Acteur implements IFabricantChocolatDeMarque{
 	
 	private List<ChocolatDeMarque>chocosProduits;
+	protected HashMap<Feve, Double> stockFeves;
+	protected HashMap<Chocolat, Double> stockChoco;
+	protected HashMap<ChocolatDeMarque, Double> stockChocoMarque;
+	protected HashMap<Feve, HashMap<Chocolat, Double>> pourcentageTransfo; // pour les differentes feves, le chocolat qu'elle peuvent contribuer a produire avec le rati
 
 	public Transformateur2() {
 		super();
+		this.chocosProduits = new LinkedList<ChocolatDeMarque>();
 	}
 	
+	public void initialiser() {
+		super.initialiser();
+		this.stockFeves=new HashMap<Feve,Double>();
+		for (Feve f : Feve.values()) {
+			this.stockFeves.put(f, 10000.0);
+			this.totalStocksFeves.ajouter(this, 10000.0, this.cryptogramme);
+			this.journal.ajouter("ajout de 10000 de "+f+" au stock de feves --> total="+this.totalStocksFeves.getValeur(this.cryptogramme));
+		}
+		this.stockChoco=new HashMap<Chocolat,Double>();
+		for (Chocolat c : Chocolat.values()) {
+			this.stockChoco.put(c, 1000.0);
+			this.totalStocksChoco.ajouter(this, 1000.0, this.cryptogramme);
+			this.journal.ajouter("ajout de 1000 de "+c+" au stock de chocolat --> total="+this.totalStocksFeves.getValeur(this.cryptogramme));
+		}
+		this.stockChocoMarque=new HashMap<ChocolatDeMarque,Double>();
+		this.pourcentageTransfo = new HashMap<Feve, HashMap<Chocolat, Double>>();
+		this.pourcentageTransfo.put(Feve.F_HQ_BE, new HashMap<Chocolat, Double>());
+		double conversion = 1.0 + (100.0 - 90.0)/100.0;
+		this.pourcentageTransfo.get(Feve.F_HQ_BE).put(Chocolat.C_HQ_BE, conversion);// la masse de chocolat obtenue est plus importante que la masse de feve vue l'ajout d'autres ingredients
+		conversion = 1.0 + (100.0 - 70.0)/100.0;
+		this.pourcentageTransfo.put(Feve.F_MQ, new HashMap<Chocolat, Double>());
+		this.pourcentageTransfo.get(Feve.F_MQ).put(Chocolat.C_MQ, conversion);
+	}
+
 	public List<ChocolatDeMarque> getChocolatsProduits() {
 		if (this.chocosProduits.size()==0) {
 				Chocolat c1 = Chocolat.C_MQ;
