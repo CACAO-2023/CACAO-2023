4dd8d8ab72f32997bc760f999207f64d494de45e
thoxy <thoxy@172.16.128.246>
2023-04-05 10:15:34 +0200

    Merge branch 'main' of https://github.com/theoo33/CACAO-2023-Eq7 into main

--- src/abstraction/eq7Distributeur1/Distributeur1AcheteurOA.java
+++ src/abstraction/eq7Distributeur1/Distributeur1AcheteurOA.java
@@ -1,10 +1,19 @@
 package abstraction.eq7Distributeur1;
 
+import java.util.List;
+
 import abstraction.eqXRomu.offresAchat.IAcheteurOA;
+import abstraction.eqXRomu.offresAchat.PropositionVenteOA;
 
 public class Distributeur1AcheteurOA extends Distributeur1 implements IAcheteurOA {
 
 	public Distributeur1AcheteurOA() {
 		super();
+	}
+
+	@Override
+	public PropositionVenteOA choisirPV(List<PropositionVenteOA> propositions) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
