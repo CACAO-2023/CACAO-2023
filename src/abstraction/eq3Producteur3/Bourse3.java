1fbcc908f3243bd3e9d897af02f4ea503fbf3515
Gabriel <Gabriel@172.16.127.64>
2023-04-10 09:51:49 +0200

    Bourse debut

--- src/abstraction/eq3Producteur3/Bourse3.java
+++ src/abstraction/eq3Producteur3/Bourse3.java
@@ -11,25 +11,24 @@ import abstraction.eqXRomu.produits.Lot;
  * @author Gabriel
  */
 public class Bourse3 extends Producteur3 implements IVendeurBourse {
-
-
+	
+	/**
+	 * @author Gabriel
+	 */
 	public double offre(Feve f, double cours) {
 		//== fonctionne ?
 		int quantiteM =0;
-		int quantiteH=0;
-		if(f.equals(Feve.F_MQ_BE)) {
-			Lot lotM = super.getStock().getStock().get(Feve.F_MQ_BE);
-			Set<Integer> KeyListM = lotM.getQuantites().keySet();
+		Lot lotM = super.getStock().getStock().get(Feve.F_MQ_BE);
+		Set<Integer> KeyListM = lotM.getQuantites().keySet();
 			for(Integer key : KeyListM) {
 				//Les feves M ont depasse les 6 mois donc il faut les vendre
 				if(Filiere.LA_FILIERE.getEtape() - key > 12) {
 					quantiteM += lotM.getQuantites().get(key);
-					boolean retirer = super.getStock().retirer(Feve.F_MQ_BE, lotM.getQuantites().get(key));
 				}
-			}
+			
 			
 		}
-		return 0;
+		return quantiteM;
 	}
 
 	
