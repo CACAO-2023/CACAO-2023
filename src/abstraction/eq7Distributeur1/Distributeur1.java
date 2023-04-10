cdb6def15bc4cc9bbd9adbead665aef6b8f89808
thoxy <thoxy@172.16.128.246>
2023-04-10 23:40:44 +0200

    Merge branch 'main' of https://github.com/CACAO-2023/CACAO-2023 into main

--- src/abstraction/eq7Distributeur1/Distributeur1.java
+++ src/abstraction/eq7Distributeur1/Distributeur1.java
@@ -21,6 +21,10 @@ public class Distributeur1 extends Distributeur1Acteur implements IDistributeurC
 		
 	}
 	
+	protected double prevision(ChocolatDeMarque marque) {
+		return 0;
+	}
+	
 	
 	
 	/**
@@ -48,7 +52,7 @@ public class Distributeur1 extends Distributeur1Acteur implements IDistributeurC
 	
 	public double prixPromotion(ChocolatDeMarque choco) {
 		double p = prix(choco);
-		if (((Filiere.LA_FILIERE.getEtape()%4)==0)&&(choco.getChocolat()!=Chocolat.C_BQ)) {
+		if (((Filiere.LA_FILIERE.getEtape()%2)==0)&&(choco.getChocolat()!=Chocolat.C_BQ)) {
 			return p*0.9;
 		}
 		else {
