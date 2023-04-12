2293d659e1d428cfab24d3b086fea6284a296718
Alexian <Alexian@172.16.126.8>
2023-04-12 11:12:34 +0200

    Merge branch 'main' of https://github.com/AlexianBtrl/CACAO-2023-Eq4 into main

--- src/abstraction/eq4Transformateur1/Transformateur1.java
+++ src/abstraction/eq4Transformateur1/Transformateur1.java
@@ -31,10 +31,6 @@ public class Transformateur1 extends AODistributeur implements IFabricantChocola
 		return this.chocosProduits;
 	}
 	
-	public void initialiser() {
-		super.initialiser();
-	}
-	
 	public void next() {
 		super.next();
 	}
