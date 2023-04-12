7ef654ef1b0310596c9a556ab62fa0739b5373ab
33699 <33699@172.16.124.75>
2023-04-12 08:34:55 +0200

    h

--- src/abstraction/eq6Transformateur3/Transformateur3Stocks.java
+++ src/abstraction/eq6Transformateur3/Transformateur3Stocks.java
@@ -1,6 +1,8 @@
 package abstraction.eq6Transformateur3;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -8,6 +10,8 @@ import abstraction.eqXRomu.filiere.Filiere;
 import abstraction.eqXRomu.produits.Chocolat;
 import abstraction.eqXRomu.produits.ChocolatDeMarque;
 import abstraction.eqXRomu.produits.Feve;
+import abstraction.eqXRomu.produits.Gamme;
+import abstraction.eqXRomu.produits.IProduit;
 import abstraction.eqXRomu.produits.Lot;
 
 public class Transformateur3Stocks extends Transformateur3Acteur  {
@@ -15,12 +19,12 @@ public class Transformateur3Stocks extends Transformateur3Acteur  {
 
 	protected Lot stockFeveBG; // feve bas gamme
     protected Lot stockFeveMG; // feve moyenne gamme
-    protected Lot stockFeveMGL;// feve moyenne gamme lab�lis�e
-    protected Lot stockFeveHGL;// feve haute gamme lab�lis�e
+    protected Lot stockFeveMGL;// feve moyenne gamme labelisée
+    protected Lot stockFeveHGL;// feve haute gamme labelisée
     protected Lot stockChocolatBG; // Chocolat bas gamme
     protected Lot stockChocolatMG; // Chocolat moyenne gamme
-    protected Lot stockChocolatMGL; // Chocolat moyenne gamme lab�lis�e
-    protected Lot stockChocolatHGL; // Chocolat haute gamme lab�lis�e
+    protected Lot stockChocolatMGL; // Chocolat moyenne gamme labelisée
+    protected Lot stockChocolatHGL; // Chocolat haute gamme labélisée
     protected List<ChocolatDeMarque> stockProduit;
 
     
@@ -42,20 +46,109 @@ public class Transformateur3Stocks extends Transformateur3Acteur  {
   * utiliser les constructeur avec le constructeur déjà ecrit
   */
   public Transformateur3Stocks() {
-	  
+      this.stockFeveBG = new Lot(Feve.F_BQ);
+      this.stockFeveMG = new Lot(Feve.F_MQ);
+      this.stockFeveMGL = new Lot(Feve.F_MQ_BE);
+      this.stockFeveHGL = new Lot(Feve.F_HQ_BE);
+      this.stockChocolatBG = new Lot(Chocolat.C_BQ);
+      this.stockChocolatMG = new Lot(Chocolat.C_MQ);
+      this.stockChocolatMGL = new Lot(Chocolat.C_MQ_BE);
+      this.stockChocolatHGL = new Lot(Chocolat.C_HQ_BE);
+      this.stockProduit = new ArrayList<ChocolatDeMarque>();
   }
-public void ajouterFeve(Feve feve, Double quantite) {
-	  
-  }
-  public void ajouterChocolat(Chocolat chocolat, Double quantite) {
-	
-  }
-  public boolean estPerimeFeve(int datePeremption) {
-	  return Filiere.LA_FILIERE.getEtape()>(datePeremption);
-  }
-  public void retirerFeve(Feve feve, double quantite)  {
-	    
+  public void ajouterFeve(Feve feve, int quantite, int dateDeRecolte) {
+	    Lot lot;
+	    switch(feve.getGamme()) {
+	        case BQ:
+	            lot = stockFeveBG;
+	            break;
+	        case MQ:
+	        	if(feve.isBioEquitable()) {
+	        		lot = stockFeveMGL;
+	        		break ;
+	        	}else {
+	        		lot = stockFeveMG;
+	                break;
+	        	}
+	        case HQ:
+	        	lot = stockFeveHGL;
+        		break ;
+	        default:
+	            throw new IllegalArgumentException("Type de fève invalide");
+	    }
+	    lot.ajouter(quantite, dateDeRecolte);
 	}
+  private Lot getLotFeve(IProduit produit) {
+	    if (produit instanceof Feve) {
+	        Feve feve = (Feve) produit;
+	        switch (feve.getGamme()) {
+	            case BQ:
+	                return stockFeveBG;
+	            case MQ:
+	                if (feve.isBioEquitable()) {
+	                    return stockFeveMGL;
+	                } else {
+	                    return stockFeveMG;
+	                }
+	            case HQ:
+	                return stockFeveHGL;
+	            default:
+	                return null;
+	        }
+	    } else {
+	        return null;
+	    }
+	}
+
+
+  public void retirerFeve(Feve feve, double quantite) {
+	    Lot lot = getLotFeve(feve);
+	    HashMap<Integer, Double> quantites = lot.getQuantites();
+
+	    // Tri des clés dans l'ordre décroissant (les plus vieilles feves en premier)
+	    List<Integer> keys = new ArrayList<Integer>(quantites.keySet());
+	    Collections.sort(keys, Collections.reverseOrder());
+
+	    double quantiteRetiree = 0;
+	    for (int key : keys) {
+	        if (quantiteRetiree == quantite) {
+	            break;
+	        }
+	        if (key >= 0 && key <= 6) { // On retire uniquement les feves périmées (plus de 6 mois)
+	            double quantiteRestante = quantites.get(key);
+	            if (quantiteRestante >= quantite - quantiteRetiree) {
+	                quantites.put(key, quantiteRestante - (quantite - quantiteRetiree));
+	                quantiteRetiree = quantite;
+	            } else {
+	                quantites.remove(key);
+	                quantiteRetiree += quantiteRestante;
+	            }
+	        }
+	    }
+	}
+  public void ajouterChocolat(Chocolat choco,int quantite,int dateProduction) {
+	  Lot lot ;
+	  switch(choco.getGamme()) {
+	  	case BQ :
+	  		lot=this.stockChocolatBG ;
+	  		break ;
+	  	case MQ :
+	  		if(choco.isBioEquitable()) {
+	  			lot=this.stockChocolatMGL ;
+	  			break ;
+	  		}else {
+	  			lot=this.stockChocolatMG ;
+	  			break ;
+	  		}
+	  	case HQ :
+	  		lot=this.stockChocolatHGL ;
+	  		break ;
+	  	default :
+	  		throw new IllegalArgumentException("Type de Chocolat invalide");
+	  }
+	  lot.ajouter(quantite,dateProduction);
+  }
+  
 
 	    
 	    
