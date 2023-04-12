e71501f7cbb5291539eefb762f2c4921084163bc
willi <willi@172.16.124.54>
2023-04-12 08:26:47 +0200

    w

--- src/abstraction/eq9Distributeur3/Distributeur3Acteur.java
+++ src/abstraction/eq9Distributeur3/Distributeur3Acteur.java
@@ -76,10 +76,16 @@ public class Distributeur3Acteur implements IActeur {
 
 
 		journal_activitegenerale.ajouter("Etape="+Filiere.LA_FILIERE.getEtape());
+		journal_activitegenerale.ajouter("Solde="+getSolde()+"€");
 		if (Filiere.LA_FILIERE.getEtape()>=1) {
 			for (int i=0; i<this.chocolats.size(); i++) {
-		//	journal.ajouter("Le prix moyen du chocolat \""+chocolats.get(i).getNom()+"\" a l'etape precedente etait de "+Filiere.LA_FILIERE.prixMoyen(chocolats.get(i), Filiere.LA_FILIERE.getEtape()-1));
-			//journal.ajouter("Les ventes de chocolat \""+chocolats.get(i)+" il y a un an etaient de "+Filiere.LA_FILIERE.getVentes(chocolats.get(i), Filiere.LA_FILIERE.getEtape()-24));
+				if(Filiere.LA_FILIERE.getEtape()-1 > 1) 
+				{
+				journal_activitegenerale.ajouter("Le prix moyen du chocolat \""+chocolats.get(i).getNom()+"\" a l'etape precedente etait de "+Filiere.LA_FILIERE.prixMoyen(chocolats.get(i), Filiere.LA_FILIERE.getEtape()-1));
+				journal_activitegenerale.ajouter("Les ventes de chocolat \""+chocolats.get(i)+" a l'etape precedente etaient de "+Filiere.LA_FILIERE.getVentes(chocolats.get(i), Filiere.LA_FILIERE.getEtape()-1));
+				}
+				
+
 			
 			
 			}
