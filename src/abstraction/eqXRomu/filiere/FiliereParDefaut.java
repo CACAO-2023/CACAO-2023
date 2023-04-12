a020b2b7cdd7a7cee4cb54a0adccc9894d1e2d5a
natha <natha@172.16.125.51>
2023-04-12 08:12:32 +0200

    Merge branch 'main' of https://github.com/NathanSlbg/NathanSlbg into main

--- src/abstraction/eqXRomu/filiere/FiliereParDefaut.java
+++ src/abstraction/eqXRomu/filiere/FiliereParDefaut.java
@@ -8,7 +8,6 @@ import abstraction.eq3Producteur3.Producteur3;
 import abstraction.eq4Transformateur1.Transformateur1;
 import abstraction.eq5Transformateur2.Transformateur2;
 import abstraction.eq6Transformateur3.Transformateur3;
-import abstraction.eq6Transformateur3.Transformateur3Stocks;
 import abstraction.eq7Distributeur1.Distributeur1;
 import abstraction.eq8Distributeur2.Distributeur2;
 import abstraction.eq9Distributeur3.Distributeur3;
