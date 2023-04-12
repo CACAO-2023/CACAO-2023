9e783ad7ae64d8c963061b0b4b2cf80c4e5eff1f
bapti <bapti@172.16.121.46>
2023-04-12 10:46:13 +0200

    baptiste augmentation des prix

--- src/abstraction/eq9Distributeur3/DistributeurChocolatDeMarque.java
+++ src/abstraction/eq9Distributeur3/DistributeurChocolatDeMarque.java
@@ -27,7 +27,7 @@ public class DistributeurChocolatDeMarque extends Distributeur3Acteur implements
 	//william
 	@Override
 	public double prix(ChocolatDeMarque choco) {
-		return 0.0;
+		return 1.0;
 	}
 	
 	//baptiste
