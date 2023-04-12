524ebfe6caeb3175ff5d1fde9088db310e11b7ae
franc <franc@MSI>
2023-04-12 08:54:57 +0200

    Merge remote-tracking branch 'origin/main' into main

--- src/abstraction/eq4Transformateur1/Achat/CC_producteur.java
+++ src/abstraction/eq4Transformateur1/Achat/CC_producteur.java
@@ -38,9 +38,7 @@ public class CC_producteur extends Transformateur1Transformateur implements IAch
 		return false;
 }
 	public Echeancier contrePropositionDeLAcheteur(ExemplaireContratCadre contrat) {
-		if (contrat.getTeteGondole()) {
-			return null;
-		}
+
 		this.journal.ajouter(COLOR_LLGRAY, Color.BLUE, "  CCA : j'accepte l'echeancier "+contrat.getEcheancier());
 		return contrat.getEcheancier();
 	}
