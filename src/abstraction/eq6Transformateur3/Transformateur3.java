202e8f07c8f3eadbc80d7696fb603dda86d872ea
natha <natha@172.16.127.59>
2023-04-12 09:33:24 +0200

    marques de choco

--- src/abstraction/eq6Transformateur3/Transformateur3.java
+++ src/abstraction/eq6Transformateur3/Transformateur3.java
@@ -1,10 +1,26 @@
 package abstraction.eq6Transformateur3;
 
-public class Transformateur3 extends Transformateur3Acteur{
+import java.util.LinkedList;
+import java.util.List;
+
+import abstraction.eqXRomu.filiere.IFabricantChocolatDeMarque;
+import abstraction.eqXRomu.produits.ChocolatDeMarque;
+
+public class Transformateur3 extends Transformateur3Acteur implements IFabricantChocolatDeMarque{
 
 	/**ecrit par Nathan Claeys
 	 */
+	protected List<ChocolatDeMarque>chocosProduits;
 		public Transformateur3 () {
 			super();
+			this.chocosProduits = new LinkedList<ChocolatDeMarque>();
 		}
+/** ecrit par Nathan Claeys
+ * @return
+ */
+	@Override
+	public List<ChocolatDeMarque> getChocolatsProduits() {
+		// TODO Auto-generated method stub
+		return this.chocosProduits;
+	}
 }
