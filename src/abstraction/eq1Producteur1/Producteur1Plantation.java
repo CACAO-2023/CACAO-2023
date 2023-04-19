4268e93892327f785060a1e28999ee3dc7886f23
charlespelong <charlespelong@dhcp-2-10.gde.emn.fr>
2023-04-19 11:08:33 +0200

    Bourse

--- src/abstraction/eq1Producteur1/Producteur1Plantation.java
+++ src/abstraction/eq1Producteur1/Producteur1Plantation.java
@@ -5,8 +5,11 @@ import java.util.HashMap;
 import java.util.concurrent.ThreadLocalRandom;
 
 import abstraction.eqXRomu.filiere.Filiere;
+import abstraction.eqXRomu.produits.Feve;
 import abstraction.eqXRomu.produits.Lot;
 public class Producteur1Plantation extends Producteur1Acteur {
+	private Lot stockBasPasSec;
+	private Lot stockMoyPasSec;
 
 //===== 4 premières methodes : elouan =====
 
@@ -16,11 +19,26 @@ public class Producteur1Plantation extends Producteur1Acteur {
 	public champ getChampMoy() {
 		return this.champMoy;
 	}
+	public Lot getStockMoy() {
+		return this.stockMoyPasSec ;
+	}
 	public Lot getStockBas() {
+		return this.stockBasPasSec ;
+	}
+	public Lot getVraiStockM() {
+		return this.stockFeveMoy;
+	}
+	public Lot getVraiStockB() {
 		return this.stockFeveBas;
 	}
-	public Lot getStockMoy() {
-		return this.stockFeveMoy ;
+	
+	public void initialiser () { // elouan
+		//initialisation d'un faible nombre de feves qui sont en sechage
+		super.initialiser();
+		this.stockBasPasSec = new Lot(Feve.F_BQ);
+		this.stockBasPasSec.ajouter(0,50);
+		this.stockMoyPasSec = new Lot(Feve.F_MQ);
+		this.stockMoyPasSec.ajouter(0,10);
 	}
 
 	
@@ -28,17 +46,19 @@ public class Producteur1Plantation extends Producteur1Acteur {
 		//===== début Elouan =====
 		super.next();
 		Lot lot_moy = this.getStockMoy();
-		HashMap<Integer, Double> quantiteFeveM = lot_moy.getQuantites() ;
+		Lot lot_moy_sec = this.getVraiStockM();
+		HashMap<Integer, Double> quantiteFeveM = lot_moy.getQuantites();
 		champ cm = this.getChampMoy();
 		for (Integer i : cm.getQuantite().keySet()) {
 			double q = cm.getQuantite().get(i);
+			Filiere.LA_FILIERE.getBanque().virer(Filiere.LA_FILIERE.getActeur("EQ1"), cryptogramme, Filiere.LA_FILIERE.getActeur("Banque"), 30*q);
 			if (step-i==2080) { //supprime l'hectar quand il produit plus, au bout de 40 ans pour la v1
 				cm.supprimer(i);
 				cm.ajouter(step, q); //on le replante quand il périme : v2 à améliorer
 			}
 				else if ((step-i)%12==0 && step-i>0) 
 				// ===== elouan et début gab =====
-				{Filiere.LA_FILIERE.getBanque().virer(Filiere.LA_FILIERE.getActeur("EQ1"), cryptogramme, Filiere.LA_FILIERE.getActeur("Banque"), 30*q);
+				{
 				double nb_tonnes = q*0.56 ; //ajouter facteur random
 				double random = ThreadLocalRandom.current().nextDouble(0.9, 1.1);
 				nb_tonnes = nb_tonnes * random ;
@@ -56,18 +76,24 @@ public class Producteur1Plantation extends Producteur1Acteur {
 		int nb_step_perime = step-12;
 		Double fevemoyabas = quantiteFeveM.get(nb_step_perime); //quantite de feve qui baisse de gamme, qu'on va rajouter dans le stock de bas
 		quantiteFeveM.remove(nb_step_perime);
+		// on s'occupe des feves sechées
+		Double nb_feve_sec = quantiteFeveM.get(step-1);
+		lot_moy_sec.ajouter(step, nb_feve_sec);
+		quantiteFeveM.remove(step-1);
 		Lot lot_bas = this.getStockBas();
-		HashMap<Integer, Double> quantiteFeveB = lot_bas.getQuantites() ;
+		HashMap<Integer, Double> quantiteFeveB = lot_bas.getQuantites();
+		Lot lot_bas_sec = this.getVraiStockB();
 		champ cb = this.getChampBas();
 		for (Integer i : cb.getQuantite().keySet()) {
 			double q = cb.getQuantite().get(i);
+			Filiere.LA_FILIERE.getBanque().virer(Filiere.LA_FILIERE.getActeur("EQ1"), cryptogramme, Filiere.LA_FILIERE.getActeur("Banque"), 30*q);
 			if (step-i==2080) { //supprime l'hectar quand il produit plus, au bout de 40 ans pour la v1
 				cb.supprimer(i);
 				cb.ajouter(step, q); //on le replante quand il périme : v2 à améliorer
 			}
 				else if ((step-i)%10==0 && step-i>0) 
 				// ===== elouan et début gab =====
-				{Filiere.LA_FILIERE.getBanque().virer(Filiere.LA_FILIERE.getActeur("EQ1"), cryptogramme, Filiere.LA_FILIERE.getActeur("Banque"), 30*q);
+				{
 				double nb_tonnes = q*0.56 ; //ajouter facteur random
 				double random = ThreadLocalRandom.current().nextDouble(0.9, 1.15);
 				nb_tonnes = nb_tonnes * random ;
@@ -86,11 +112,18 @@ public class Producteur1Plantation extends Producteur1Acteur {
 			quantiteFeveB.put(step-6, fevemoyabas); //on rajoute les feves qui ont baisse de qualite pour qu'elles aient une duree de vie de 3 mois
 		}
 		quantiteFeveB.remove(nb_step_perime);
-		Filiere.LA_FILIERE.getBanque().virer(Filiere.LA_FILIERE.getActeur("EQ1"), cryptogramme, Filiere.LA_FILIERE.getActeur("Banque"), (this.getStockBas().getQuantiteTotale()+this.getStockMoy().getQuantiteTotale())*50);
+		// on s'occupe des feves sechées
+		Double nb_feve_sec2 = quantiteFeveB.get(step-1);
+		lot_bas_sec.ajouter(step, nb_feve_sec2);
+		quantiteFeveB.remove(step-1);
+		Double q1 = 0.0;
+		Double q2 = 0.0;
+		if (lot_moy_sec!=null) 
+			{q1=lot_moy_sec.getQuantiteTotale();}
+		if (lot_bas_sec!=null) 
+			{q2=lot_moy_sec.getQuantiteTotale();}
+		Filiere.LA_FILIERE.getBanque().virer(Filiere.LA_FILIERE.getActeur("EQ1"), cryptogramme, Filiere.LA_FILIERE.getActeur("Banque"), (q1+q2)*50);
 		// cout de stockage
 		}
-	
-		
 		//===== fin elouan et gab =====	
 	}
-
--- src/abstraction/eq1Producteur1/ProducteurVendeurCC.java
+++ src/abstraction/eq1Producteur1/ProducteurVendeurCC.java
@@ -1,7 +1,9 @@
 package abstraction.eq1Producteur1;
 
+import abstraction.eqXRomu.contratsCadres.ContratCadre;
 import abstraction.eqXRomu.contratsCadres.Echeancier;
 import abstraction.eqXRomu.contratsCadres.ExemplaireContratCadre;
+import abstraction.eqXRomu.contratsCadres.IAcheteurContratCadre;
 import abstraction.eqXRomu.contratsCadres.IVendeurContratCadre;
 import abstraction.eqXRomu.contratsCadres.SuperviseurVentesContratCadre;
 import abstraction.eqXRomu.filiere.Filiere;
@@ -74,40 +76,35 @@ public class ProducteurVendeurCC extends Producteur1Plantation implements IVende
 	@Override
 	public double propositionPrix(ExemplaireContratCadre c) {
 		double p=0;
-		switch((Feve)c.getProduit()) {		
-		case F_BQ:
-			p= 1100*c.getQuantiteTotale();
-		case F_MQ:
-			p= 1300*c.getQuantiteTotale();
-		case F_HQ_BE : p= 0;
-		case F_MQ_BE : p= 0;
+		if((Feve)c.getProduit()==Feve.F_BQ) {		
+			p= 1.1*c.getQuantiteTotale();
+		if((Feve)c.getProduit()==Feve.F_MQ)
+			p= 1.3*c.getQuantiteTotale();
 		}
 		return p;
 		
 		
 	}
 
+	
 	@Override
 	public double contrePropositionPrixVendeur(ExemplaireContratCadre c) {
-<<<<<<< HEAD
-		if (c.getPrix()>propositionPrix(c)) {//s'ils sont genereux pourquoi pas :)
-=======
 		
 		
 		double p= (c.getPrix()+ propositionPrix(c))/2;		
 		if (c.getPrix()>=propositionPrix(c)) {
->>>>>>> branch 'main' of https://github.com/Charles141002/CACAO-2023
 			return c.getPrix();
 		}
 		else {
-		double p= (c.getPrix()+propositionPrix(c))/2;
+		
 		if (p>0.75*propositionPrix(c)) {
 		return p;
 		}else {
-			return (p+propositionPrix(c))/2;
+			return propositionPrix(c)*Math.random()+ propositionPrix(c);
 		}}
+		
 	}
-
+	
 	@Override
 	public void notificationNouveauContratCadre(ExemplaireContratCadre contrat) {
 		this.mescontrats.add(contrat);
@@ -143,12 +140,9 @@ public class ProducteurVendeurCC extends Producteur1Plantation implements IVende
 			}
 		}
 		this.mescontrats.removeAll(contratstermine);
-<<<<<<< HEAD
-=======
 		this.PropositionVendeur(Feve.F_BQ);
 		this.PropositionVendeur(Feve.F_MQ);
 		this.journal_ventes.ajouter("Nos contrats à l'étape "+ Filiere.LA_FILIERE.getEtape()+"sont "+ this.mescontrats);
->>>>>>> branch 'main' of https://github.com/Charles141002/CACAO-2023
 	}
 
 	@Override
@@ -157,8 +151,6 @@ public class ProducteurVendeurCC extends Producteur1Plantation implements IVende
 		return false;
 	}
 	
-<<<<<<< HEAD
-=======
 	public ExemplaireContratCadre PropositionVendeur(IProduit produit){
 		
 		IAcheteurContratCadre client=supCCadre.getAcheteurs(produit).get((int)supCCadre.getAcheteurs(produit).size()-1);
@@ -173,6 +165,5 @@ public class ProducteurVendeurCC extends Producteur1Plantation implements IVende
 		
 	}
 	
->>>>>>> branch 'main' of https://github.com/Charles141002/CACAO-2023
 
 }
