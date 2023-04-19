6236078dd2adf77309f02bcf3691fe61ba813c54
user <user@dhcp-121-24.mde.emn.fr>
2023-04-19 09:36:18 +0200

    fin de producteur planteur

--- src/abstraction/eq2Producteur2/Producteur2ASProducteurPlanteur.java
+++ src/abstraction/eq2Producteur2/Producteur2ASProducteurPlanteur.java
@@ -18,7 +18,11 @@ public class Producteur2ASProducteurPlanteur extends Producteur2AStockeur{
 	public HashMap<Feve, Double> salaires;
 	public HashMap<Feve, Integer> surface_plantation;
 	public HashMap<Feve, Double> prix;
-	public HashMap<Feve,HashMap<Integer, Integer>> age_hectares;
+	public HashMap<Feve,HashMap<Integer, Integer>> age_hectares; 
+	// Pour age_hectares nous avons un Hashmap dans un autre, ici la première clef fait reference à la Feve car 
+	//Les employes auront differents salaires selon la gamme sur laquelle ils travaillent
+	//La deuxiemes clef est un entirer qui correspond à l'age (en step) des hectares
+	//Ainsi pour chaque type de feve et chaque age on connait le nombre d'hectares que l'on possède
 
 	public Producteur2ASProducteurPlanteur() {
 		super();
@@ -29,7 +33,8 @@ public class Producteur2ASProducteurPlanteur extends Producteur2AStockeur{
 		for (Feve f: this.lesFeves)
 			this.age_hectares.put(f, new HashMap<Integer, Integer>());
 	}
-		
+	// Mise en place des differents setters
+	
 	public HashMap<Feve, Integer> getEmployes(){
 		return this.employes;
 	}
@@ -47,8 +52,8 @@ public class Producteur2ASProducteurPlanteur extends Producteur2AStockeur{
 	public HashMap<Feve,HashMap<Integer, Integer>> get_Age_Hectares(){
 		return this.age_hectares;
 	}
-	// Mise en place des differents setters qui pour la version 1 ne sont pas encore utilies car seuls les 
-	//plantations évoluent cependant 
+	// Mise en place des differents setters qui pour la version 1 ne sont pas encore tous utilies car seuls les 
+	//plantations et le nombre d'employes evoluent 
 	//
 	public void setEmploye(int employes_BQ, int employes_MQ, int employes_MQ_BE, int employes_HQ_BE) {
 		this.employes.put(Feve.F_BQ, employes_BQ);
@@ -56,6 +61,13 @@ public class Producteur2ASProducteurPlanteur extends Producteur2AStockeur{
 		this.employes.put(Feve.F_MQ_BE, employes_MQ_BE);
 		this.employes.put(Feve.F_HQ_BE, employes_HQ_BE);
 	}
+	/**
+	 * @author Florian
+	 * @param salaire_employes_BQ
+	 * @param salaire_employes_MQ
+	 * @param salaire_employes_MQ_BE
+	 * @param salaire_employes_HQ_BE
+	 */
 	public void setSalaires(double salaire_employes_BQ, double salaire_employes_MQ, double salaire_employes_MQ_BE, double salaire_employes_HQ_BE) {
 		this.salaires.put(Feve.F_BQ, salaire_employes_BQ);
 		this.salaires.put(Feve.F_MQ,salaire_employes_MQ);
@@ -86,18 +98,11 @@ public class Producteur2ASProducteurPlanteur extends Producteur2AStockeur{
 		
 		
 	//}
-	public void next() {
-		super.next();
-		this.CoutProd();
-		this.Prevision_Vente();
-		this.ajustement_plantation();
-		this.Prevision_Production(Filiere.LA_FILIERE.getEtape());
-		for (Feve f : this.age_hectares.keySet()) {
-			this.ajouterStock(f, Filiere.LA_FILIERE.getEtape(), Prevision_Production(Filiere.LA_FILIERE.getEtape()).get(f));	
-		}
-		
-		
-	}
+	
+	//La fonction ci_dessous prevois les quantities de cacao que l'on sera apte a produire à une step donnée
+	//Avec nos terres actuelles. En effet dans notre cas un hectare met 3 ans pour que les cacaoyers dessus
+	//puissent produire des feves, ils produisent des feves de maniere constante jusqu'a 40 ans
+	
 	public HashMap<Feve, Double> Prevision_Production(int step){
 		HashMap<Feve, Double> prevision_production = new HashMap<Feve, Double>();
 		for (Feve f : this.age_hectares.keySet()) {
@@ -118,12 +123,18 @@ public class Producteur2ASProducteurPlanteur extends Producteur2AStockeur{
 		return prevision_production;	
 	}
 	
+	// Cette fonction calcul les côut de production de chaque feve, on prend uniquement la masse salariale
+	// dediee a chaque feve
+	
 	public HashMap<Feve, Double> CoutProd(){
 		HashMap<Feve, Double> coûts_totaux = new HashMap<Feve, Double>();
 		for (Feve f : this.salaires.keySet()) {
 			coûts_totaux.put(f, this.salaires.get(f)*this.employes.get(f)); }
 		return coûts_totaux;
 	}
+	
+	//Pour prevoir les ventes on se base sur la somme des contrats cadre et la quantite vendue en bourse
+	
 	public HashMap<Feve, Double> Prevision_Vente(){
 		 HashMap<Feve, Double> prevision_vente = new  HashMap<Feve, Double>();
 		 prevision_vente.put(Feve.F_BQ, BQquantiteVendueBourse.getValeur());
@@ -132,6 +143,10 @@ public class Producteur2ASProducteurPlanteur extends Producteur2AStockeur{
 		 prevision_vente.put(Feve.F_HQ_BE, aLivrerStep(Filiere.LA_FILIERE.getEtape(), Feve.F_HQ_BE));
 		return prevision_vente;	
 	}
+	
+	//Cette premiere fonction de rentrabilite permet de savoir pour une feve donne sur la prix choisi permet
+	//une rentabilite superieure a 10%
+	
 	public boolean Rentabilites(Feve f, Double prix){
 		double rentabilite = prix*Prevision_Production(Filiere.LA_FILIERE.getEtape()).get(f)/CoutProd().get(f);
 		if (rentabilite>=1.1) {
@@ -139,9 +154,15 @@ public class Producteur2ASProducteurPlanteur extends Producteur2AStockeur{
 		}
 		return false;	
 	}
+	
+	//Cette fonction est legerement differente, ici on ne renvoit pas un bouleen mais selon la feve la fonction
+	//nous renvoit le prix minimum de rentabilite de celle-ci
+	
 	public double prix_rentable(Feve f) {
 		return 1.1*CoutProd().get(f)/Prevision_Production(Filiere.LA_FILIERE.getEtape()).get(f);
 	}
+	
+	//
 	public void ajustement_plantation() {
 		for (Feve f : this.salaires.keySet()) {
 			int nb_a_planter = 0;
@@ -149,17 +170,46 @@ public class Producteur2ASProducteurPlanteur extends Producteur2AStockeur{
 				if (Filiere.LA_FILIERE.getEtape()-i>= 37*24) {
 					nb_a_planter =+ 1;
 				}
+			if (Filiere.LA_FILIERE.getEtape()%24==0) {
+				nb_a_planter =(int) + Math.round(Filiere.LA_FILIERE.getEtape()*0.03);
+			}
 			}
 			Planter(f, nb_a_planter);	
 		}
 	}
+	// Une fois les plantation ajustees, sachant qu'un employe s'occupe d'un hectare on adapte le nombre
+	//d'employes aux nombres d'hectares
 	public void ajustement_employes () {
-		HashMap(Feve)
-		for (Feve f : this.age_hectares.keySet()) {
-			
+		HashMap<Feve,Integer> B = new HashMap<Feve,Integer>();
+		for (Feve f : this.age_hectares.keySet()){
+			int A=0;
+			for (int i=3*24 ; i<age_hectares.get(f).size(); i++) {
+				A=+age_hectares.get(f).get(i);
+			}
+			if (f == Feve.F_MQ_BE ||f == Feve.F_HQ_BE ) {
+				if (A<0.95*this.employes.get(f)) {
+					A = (int) Math.ceil(0.95*this.employes.get(f));
+				}
+			}
+			B.put(f, A);	
 		}
-		
+		setEmploye(B.get(Feve.F_BQ), B.get(Feve.F_MQ), B.get(Feve.F_MQ_BE), B.get(Feve.F_HQ_BE));	
 	}
+	
+	public void next() {
+		super.next();
+		this.CoutProd();
+		this.Prevision_Vente();
+		this.ajustement_plantation();
+		this.Prevision_Production(Filiere.LA_FILIERE.getEtape());
+		for (Feve f : this.age_hectares.keySet()) {
+			if (Prevision_Production(Filiere.LA_FILIERE.getEtape()).get(f)!=0){
+			this.ajouterStock(f, Filiere.LA_FILIERE.getEtape(), Prevision_Production(Filiere.LA_FILIERE.getEtape()).get(f));
+		}
+		this.ajustement_employes();
+		}
+	}
+	
 	
 	
 	// Classe calculant la production et les coûts de production, et gère les stocks de notre acteur
