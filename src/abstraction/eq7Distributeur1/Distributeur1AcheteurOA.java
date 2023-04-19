978add4d2f6351a4c4fb8f1b96493bb7af77edb4
thoxy <thoxy@HUAWEI-MateBook-D14>
2023-04-19 09:54:10 +0200

    ras

--- src/abstraction/eq7Distributeur1/Distributeur1AcheteurOA.java
+++ src/abstraction/eq7Distributeur1/Distributeur1AcheteurOA.java
@@ -81,7 +81,7 @@ public class Distributeur1AcheteurOA extends DistributeurContratCadreAcheteur im
 		if (supOA==null) {
 			supOA =(SuperviseurVentesOA)(Filiere.LA_FILIERE.getActeur("Sup.OA"));
 		}
-		if (besoin()!=null) { //Si on manque à l'instant t de stock, on lance cette méthode d'achat car l'effet est immediat
+		if (besoin()==true) { //Si on manque à l'instant t de stock, on lance cette méthode d'achat car l'effet est immediat
 			List<ChocolatDeMarque> marque = besoinMarque();
 			HashMap<ChocolatDeMarque,Double> qte = besoinQte();
 			for (ChocolatDeMarque m : marque) {
