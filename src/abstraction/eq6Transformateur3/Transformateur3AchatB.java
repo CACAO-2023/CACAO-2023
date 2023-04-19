3be4d4a15dce7c77e34b2524f06cad8d8fd665d3
natha <natha@172.16.127.59>
2023-04-19 15:12:10 +0200

    m

--- src/abstraction/eq6Transformateur3/Transformateur3AchatB.java
+++ src/abstraction/eq6Transformateur3/Transformateur3AchatB.java
@@ -18,10 +18,10 @@ public class Transformateur3AchatB extends Transformateur3AchatCC implements IAc
 	
 	public Transformateur3AchatB () {
 		super();
-		this.coursmaxBG = new Variable ("cours maximal BG","cours maximal que l'acteur va accepter pour les feves bas de gamme",this,0.0,5000,1300);
-		this.coursmaxMG = new Variable ("cours maximal MG","cours maximal que l'acteur va accepter pour les feves moyenne gamme",this,0.0,6000,1900);
-		this.coursmaxMGL = new Variable ("cours maximal MGL","cours maximal que l'acteur va accepter pour les feves moyenne gamme labelisees",this,0.0,6000,2000);
-		this.coursmaxHGL = new Variable ("cours maximal HGL","cours maximal que l'acteur va accepter pour les feves haut de gamme",this,0.0,8000,2500);
+		this.coursmaxBG = new Variable ("cours maximal BG","cours maximal que l'acteur va accepter pour les feves bas de gamme",this,0.0,5000,5000);
+		this.coursmaxMG = new Variable ("cours maximal MG","cours maximal que l'acteur va accepter pour les feves moyenne gamme",this,0.0,6000,5900);
+		this.coursmaxMGL = new Variable ("cours maximal MGL","cours maximal que l'acteur va accepter pour les feves moyenne gamme labelisees",this,0.0,6000,6000);
+		this.coursmaxHGL = new Variable ("cours maximal HGL","cours maximal que l'acteur va accepter pour les feves haut de gamme",this,0.0,8000,6500);
 	}
 	/**
 	 * ecrit par Nathan Claeys
