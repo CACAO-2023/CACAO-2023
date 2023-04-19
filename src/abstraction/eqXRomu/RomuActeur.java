2c48c22177c6b5056783c1d1d7036c813b8ac401
eloua <eloua@172.16.127.131>
2023-04-19 10:59:56 +0200

    f

--- src/abstraction/eqXRomu/RomuActeur.java
+++ src/abstraction/eqXRomu/RomuActeur.java
@@ -102,7 +102,7 @@ public class RomuActeur implements IActeur, IMarqueChocolat {
 	@Override
 	public void next() {
 		// TODO Auto-generated method stub
-
+			
 	}
 
 	public List<Variable> getIndicateurs() {
