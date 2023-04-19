3879ebbd0653c540da55a5b448dd90e8f76f661c
asent <asent@172.16.125.201>
2023-04-19 09:04:21 +0200

    9h04

--- src/abstraction/eq7Distributeur1/Distributeur1AcheteurOA.java
+++ src/abstraction/eq7Distributeur1/Distributeur1AcheteurOA.java
@@ -81,7 +81,6 @@ public class Distributeur1AcheteurOA extends DistributeurContratCadreAcheteur im
 		if (supOA==null) {
 			supOA =(SuperviseurVentesOA)(Filiere.LA_FILIERE.getActeur("Sup.OA"));
 		}
-		journal.ajouter(""+besoin());
 		if (besoin()!=null) { //Si on manque à l'instant t de stock, on lance cette méthode d'achat car l'effet est immediat
 			List<ChocolatDeMarque> marque = besoinMarque();
 			HashMap<ChocolatDeMarque,Double> qte = besoinQte();
