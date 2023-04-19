c2d38508b39d3a5087eff4f4f36da930d2c3d8f9
marie <marie@172.16.128.161>
2023-04-19 10:57:03 +0200

    commit

--- src/abstraction/eq8Distributeur2/ContratCadre.java
+++ src/abstraction/eq8Distributeur2/ContratCadre.java
@@ -17,13 +17,12 @@ import abstraction.eqXRomu.produits.Lot;
 
 public class ContratCadre extends Distributeur2Acteur implements IAcheteurContratCadre {
 
-	public void initialiser() {
-	
-		
+	public void initialiser() {	
 	}
 
 	//Auteur : Marzougui Mariem
 	public boolean achete(IProduit produit) {
+		this.journal_achats.ajouter("on achète le produit"+produit.toString());
 		return true;
 	}
 
@@ -36,27 +35,32 @@ public class ContratCadre extends Distributeur2Acteur implements IAcheteurContra
 	//Auteur : Marzougui Mariem
 	public Echeancier contrePropositionDeLAcheteur(ExemplaireContratCadre contrat) {
 		if (contrat.getProduit() instanceof ChocolatDeMarque) {
-		ChocolatDeMarque produit = (ChocolatDeMarque) contrat.getProduit();
-		 if (produit != null && this.stocks.getStock(produit) != 0.0) {
-		 double quantiteEnStock = this.stocks.getStock(produit);
-		 if (contrat.getEcheancier().getQuantiteTotale() < quantiteEnStock) {
-		 if (Math.random() < 0.1) {
-		 return contrat.getEcheancier(); // on ne cherche pas a negocier sur le previsionnel de livraison
-		 } else { //dans 90% des cas on fait une contreproposition pour l'echeancier
-		 Echeancier e = contrat.getEcheancier();
-		 e.set(e.getStepDebut(), e.getQuantite(e.getStepDebut()) / 2.0); // on souhaite livrer deux fois moins lors de la 1ere livraison... un choix arbitraire, juste pour l'exemple...
-		 return e;
-		 }
-		 } else {
-		 return null; // on est frileux : on ne s'engage dans un contrat cadre que si on a toute la quantite en stock (on pourrait accepter même si nous n'avons pas tout car nous pouvons produire/acheter pour tenir les engagements)
-		 }
-		 } else {
-			 return null; // on ne vend pas de ce produit
-		 }}else {
-			 return null;
-		 }
-		}
-	
+			ChocolatDeMarque produit = (ChocolatDeMarque) contrat.getProduit();
+			if (produit != null && this.stocks.getStock(produit) != 0.0) {
+				double quantiteEnStock = this.stocks.getStock(produit);
+				if (contrat.getEcheancier().getQuantiteTotale() < quantiteEnStock) {
+					if (Math.random() < 0.1) {
+						this.notificationNouveauContratCadre(contrat);
+						return contrat.getEcheancier(); // on ne cherche pas a negocier sur le previsionnel de livraison
+					} else { //dans 90% des cas on fait une contreproposition pour l'echeancier
+						Echeancier e = contrat.getEcheancier();
+						e.set(e.getStepDebut(), e.getQuantite(e.getStepDebut()) / 2.0); // on souhaite livrer deux fois moins lors de la 1ere livraison... un choix arbitraire, juste pour l'exemple...
+						this.notificationNouveauContratCadre(contrat);
+						return e;
+					}
+				} else {
+					this.journal_ContratCadre.ajouter("rejet du contrat:"+contrat.toString());
+					return null; // on est frileux : on ne s'engage dans un contrat cadre que si on a toute la quantite en stock (on pourrait accepter même si nous n'avons pas tout car nous pouvons produire/acheter pour tenir les engagements)
+				}
+			} else {
+				this.journal_ContratCadre.ajouter("rejet du contrat:"+contrat.toString());
+				return null; // on ne vend pas de ce produit
+			}}else {
+				this.journal_ContratCadre.ajouter("le produit de ce contrat ne correspond pas à une marque de chocolat"+contrat.toString());
+				return null;
+			}
+	}
+
 
 	//Auteur : Marzougui Mariem
 	//On retourne le prix sans négociation
@@ -66,14 +70,17 @@ public class ContratCadre extends Distributeur2Acteur implements IAcheteurContra
 
 	//Auteur : Marzougui Mariem
 	public void notificationNouveauContratCadre(ExemplaireContratCadre contrat) {
-		this.journal_ContratCadre.ajouter(contrat.toString());	
-		
+		this.journal_ContratCadre.ajouter("contrat effectué:"+contrat.toString());	
+	}
+
+	public void next() {
+		super.next();	
 	}
 
 	//Auteur : Marzougui Mariem
 	public void receptionner(Lot lot, ExemplaireContratCadre contrat) {
 		stocks.ajouterAuStock((ChocolatDeMarque)(contrat.getProduit()),lot.getQuantiteTotale() );
-		
+
 	}
 
 }
