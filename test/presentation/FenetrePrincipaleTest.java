f1576d82b583f10539673c31d23a1fd7c8c765bc
Massimo Tisi <massimo.tisi@imt-atlantique.fr>
2023-04-29 11:15:45 +0200

    Update FenetrePrincipaleTest.java

--- test/presentation/FenetrePrincipaleTest.java
+++ test/presentation/FenetrePrincipaleTest.java
@@ -15,10 +15,8 @@ public class FenetrePrincipaleTest {
 		FenetrePrincipale fp = new FenetrePrincipale(new String[0]);
 		for (int i=0; i<100; i++)
 			((JButton) ((JPanel) fp.getRootPane().getContentPane().getComponent(2)).getComponent(0)).doClick();
-		
 		for (int i=0; i<10; i++)
 			((JButton) ((JPanel) fp.getRootPane().getContentPane().getComponent(2)).getComponent(1)).doClick();
-		
 		((JButton) ((JPanel) fp.getRootPane().getContentPane().getComponent(2)).getComponent(2)).doClick();
 	}
 
