6ea0030012375bc6a6b5903a1a87c06facb3cd15
Massimo Tisi <massimo.tisi@imt-atlantique.fr>
2023-05-03 17:30:44 +0200

    Improved JSON formatting

--- test/presentation/FenetrePrincipaleTest.java
+++ test/presentation/FenetrePrincipaleTest.java
@@ -25,15 +25,15 @@ public class FenetrePrincipaleTest {
 		((JButton) ((JPanel) fp.getRootPane().getContentPane().getComponent(2)).getComponent(2)).doClick();
 		
 		System.out.print("tempsEquipes={");
-		System.out.print("\"eq1Producteur1\":\""+ Filiere.LA_FILIERE.tempsEquipes.get("EQ1") +"\",");
-		System.out.print("\"eq2Producteur2\":\""+ Filiere.LA_FILIERE.tempsEquipes.get("EQ2") +"\",");
-		System.out.print("\"eq3Producteur3\":\""+ Filiere.LA_FILIERE.tempsEquipes.get("EQ3") +"\",");
-		System.out.print("\"eq4Transformateur1\":\""+ Filiere.LA_FILIERE.tempsEquipes.get("EQ4") +"\",");
-		System.out.print("\"eq5Transformateur2\":\""+ Filiere.LA_FILIERE.tempsEquipes.get("EQ5") +"\",");
-		System.out.print("\"eq6Transformateur3\":\""+ Filiere.LA_FILIERE.tempsEquipes.get("EQ6") +"\",");
-		System.out.print("\"eq7Distributeur1\":\""+ Filiere.LA_FILIERE.tempsEquipes.get("EQ7") +"\",");
-		System.out.print("\"eq8Distributeur2\":\""+ Filiere.LA_FILIERE.tempsEquipes.get("EQ8") +"\",");
-		System.out.print("\"eq9Distributeur3\":\""+ Filiere.LA_FILIERE.tempsEquipes.get("EQ9") +"\"}");
+		System.out.print("\"eq1Producteur1\":"+ Filiere.LA_FILIERE.tempsEquipes.get("EQ1") +",");
+		System.out.print("\"eq2Producteur2\":"+ Filiere.LA_FILIERE.tempsEquipes.get("EQ2") +",");
+		System.out.print("\"eq3Producteur3\":"+ Filiere.LA_FILIERE.tempsEquipes.get("EQ3") +",");
+		System.out.print("\"eq4Transformateur1\":"+ Filiere.LA_FILIERE.tempsEquipes.get("EQ4") +",");
+		System.out.print("\"eq5Transformateur2\":"+ Filiere.LA_FILIERE.tempsEquipes.get("EQ5") +",");
+		System.out.print("\"eq6Transformateur3\":"+ Filiere.LA_FILIERE.tempsEquipes.get("EQ6") +",");
+		System.out.print("\"eq7Distributeur1\":"+ Filiere.LA_FILIERE.tempsEquipes.get("EQ7") +",");
+		System.out.print("\"eq8Distributeur2\":"+ Filiere.LA_FILIERE.tempsEquipes.get("EQ8") +",");
+		System.out.print("\"eq9Distributeur3\":"+ Filiere.LA_FILIERE.tempsEquipes.get("EQ9") +"}");
 		System.out.println();
 		
 	}
