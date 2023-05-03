bce0ea68912f4c773156ba4ea92aa5db5a15cb3d
Fouad <Fouad@172.16.125.20>
2023-05-03 10:05:53 +0200

    Résolution pb transformateur

--- src/abstraction/eq4Transformateur1/Transformateur1Transformateur.java
+++ src/abstraction/eq4Transformateur1/Transformateur1Transformateur.java
@@ -42,13 +42,6 @@ public class Transformateur1Transformateur extends Stock implements IFabricantCh
 
 	public void initialiser() {
 		super.initialiser();
-		this.pourcentageTransfo = new HashMap<Feve, HashMap<Chocolat, Double>>();
-		this.pourcentageTransfo.put(Feve.F_HQ_BE, new HashMap<Chocolat, Double>());
-		double conversion = 1.06;
-		this.pourcentageTransfo.get(Feve.F_HQ_BE).put(Chocolat.C_HQ_BE, conversion);// la masse de chocolat obtenue est plus importante que la masse de feve vue l'ajout d'autres ingredients
-		this.pourcentageTransfo.put(Feve.F_BQ, new HashMap<Chocolat, Double>());
-		double conversionb = 1.58;
-		this.pourcentageTransfo.get(Feve.F_BQ).put(Chocolat.C_BQ, conversionb);
 	}
 
 	/**
@@ -62,19 +55,21 @@ public class Transformateur1Transformateur extends Stock implements IFabricantCh
 		Feve fb = Feve.F_BQ;
 		Chocolat cb = Chocolat.C_BQ;
 		int transfo = (int) (Math.min(this.stockFeves.get(fb), Math.random()*1000));
+		double conversionb = 1.58;
 		if (transfo>0) {
 			this.stockFeves.put(fb, this.stockFeves.get(fb)-transfo);
 			this.totalStocksFeves.retirer(this, transfo, this.cryptogramme);
-			this.stockChoco.put(cb, this.stockChoco.get(cb)+(transfo)*this.pourcentageTransfo.get(fb).get(cb));
+			this.stockChoco.put(cb, this.stockChoco.get(cb)+(transfo)*conversionb);
 			int pourcentageCacao =  42;
-			this.totalStocksChoco.ajouter(this, ((transfo)*this.pourcentageTransfo.get(fb).get(cb)), this.cryptogramme);
-			this.journal.ajouter(COLOR_LLGRAY, Color.PINK, "Transfo de "+(transfo<10?" "+transfo:transfo)+" T de "+fb+" en "+Journal.doubleSur(transfo*this.pourcentageTransfo.get(fb).get(cb),3,2)+" T de "+cb);
+			this.totalStocksChoco.ajouter(this, ((transfo)*conversionb), this.cryptogramme);
+			this.journal.ajouter(COLOR_LLGRAY, Color.PINK, "Transfo de "+(transfo)+" T de "+fb+" en "+transfo*conversionb+" T de "+cb);
 			this.journal.ajouter(COLOR_LLGRAY, COLOR_BROWN," stock("+fb+")->"+this.stockFeves.get(fb));
 			this.journal.ajouter(COLOR_LLGRAY, COLOR_BROWN," stock("+cb+")->"+this.stockChoco.get(cb));
 		}
 		Feve fh = Feve.F_HQ_BE;
 		Chocolat ch = Chocolat.C_HQ_BE;
 		int transfoh = (int) (Math.min(this.stockFeves.get(fh), Math.random()*1000));
+		double conversion = 1.06;
 		if (transfoh>0) {
 			this.stockFeves.put(fh, this.stockFeves.get(fh)-transfoh);
 			this.totalStocksFeves.retirer(this, transfoh, this.cryptogramme);
@@ -82,9 +77,9 @@ public class Transformateur1Transformateur extends Stock implements IFabricantCh
 			int pourcentageCacao =  94;
 			ChocolatDeMarque cm= new ChocolatDeMarque(ch, "Vccotioi", pourcentageCacao, 15);
 			double scm = this.stockChocoMarque.keySet().contains(cm) ?this.stockChocoMarque.get(cm) : 0.0;
-			this.stockChocoMarque.put(cm, scm+((transfoh)*this.pourcentageTransfo.get(fh).get(ch)));
-			this.totalStocksChocoMarque.ajouter(this, ((transfoh)*this.pourcentageTransfo.get(fh).get(ch)), this.cryptogramme);
-			this.journal.ajouter(COLOR_LLGRAY, Color.PINK, "Transfo de "+(transfoh<10?" "+transfoh:transfoh)+" T de "+fh+" en "+Journal.doubleSur(transfo*this.pourcentageTransfo.get(fh).get(ch),3,2)+" T de "+ch);
+			this.stockChocoMarque.put(cm, scm+((transfoh)*conversion));
+			this.totalStocksChocoMarque.ajouter(this, ((transfoh)*conversion), this.cryptogramme);
+			this.journal.ajouter(COLOR_LLGRAY, Color.PINK, "Transfo de "+(transfoh)+" T de "+fh+" en "+transfoh*conversion+" T de "+ch);
 			this.journal.ajouter(COLOR_LLGRAY, COLOR_BROWN," stock("+fh+")->"+this.stockFeves.get(fh));
 			this.journal.ajouter(COLOR_LLGRAY, COLOR_BROWN," stock("+cm+")->"+this.stockChocoMarque.get(cm));
 		}
