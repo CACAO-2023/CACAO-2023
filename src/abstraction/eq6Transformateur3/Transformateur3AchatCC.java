4ea672c6ccf6b9ba4c7e5855cc95f9be5e901702
natha <natha@172.16.127.59>
2023-05-03 10:33:39 +0200

    m

--- src/abstraction/eq6Transformateur3/Transformateur3AchatCC.java
+++ src/abstraction/eq6Transformateur3/Transformateur3AchatCC.java
@@ -243,18 +243,14 @@ public class Transformateur3AchatCC extends Transformateur3Transformation  imple
  * return null sinon
  */
 
-	public ExemplaireContratCadre chercheContrat(IProduit produit) {
+	public void chercheContrat(IProduit produit) {
 		if (superviseur != null) {
 		List<IVendeurContratCadre> vendeurs = superviseur.getVendeurs(produit);
 		if (vendeurs.size()!=0) {
-			IVendeurContratCadre vendeur = vendeurs.get(Math.subtractExact(0, vendeurs.size()));
+			for (IVendeurContratCadre vendeur : vendeurs) {
 			super.journalAchatB.ajouter("on essaie de demander un contrat à l'equipe :"+vendeur.getNom());
 			ExemplaireContratCadre contrat = superviseur.demandeAcheteur(this, vendeur, produit, new Echeancier(Filiere.LA_FILIERE.getEtape()+1,Filiere.LA_FILIERE.getEtape()+5,100.0), super.cryptogramme, false);
-			if (contrat != null) {super.journalAchatCC.ajouter("CC cherché et trouvé :"+contrat.toString());}
-			return contrat;	
-		}
-		else {return null;}}
-		else {return null;}
+			if (contrat != null) {super.journalAchatCC.ajouter("CC cherché et trouvé :"+contrat.toString());}}}}
 	}
 	/**ecrit par Nathan Claeys
 	   * pour pouvoir rendre les variables qui peuvent aider à la prise de decision
