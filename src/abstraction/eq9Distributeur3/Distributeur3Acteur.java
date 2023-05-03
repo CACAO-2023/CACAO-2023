33ba397ee200e876617e69b81d8bae339bce3c99
willi <willi@172.16.124.54>
2023-05-03 08:50:19 +0200

    w

--- src/abstraction/eq9Distributeur3/Distributeur3Acteur.java
+++ src/abstraction/eq9Distributeur3/Distributeur3Acteur.java
@@ -29,7 +29,7 @@ public class Distributeur3Acteur implements IActeur{
 	protected Journal journal_stock;
 	protected Journal journal_activitegenerale;
 	protected List<ChocolatDeMarque> chocolats;
-	protected List<String> chocolats_cible_noms;
+
 	protected HashMap<ChocolatDeMarque, Double[]> prixMoyen;
 	protected boolean initialise = true;
 	protected double prix;
@@ -42,14 +42,7 @@ public class Distributeur3Acteur implements IActeur{
 		this.chocolats = new LinkedList<ChocolatDeMarque>();
 		
 		// william : pour pouvoir acheter le chocolat qui nous intéresse (HQ BE, MQ BE, MQ)
-		this.chocolats_cible_noms = new LinkedList<String>();
-		this.chocolats_cible_noms.add("C_HQ_BE_Vccotioi");
-		this.chocolats_cible_noms.add("C_HQ_BE_Maison_Doutre");
-		this.chocolats_cible_noms.add("C_HQ_BE_Choc");
-		this.chocolats_cible_noms.add("C_MQ_BE chokchoco bio");
-		this.chocolats_cible_noms.add("C_HQ_BE_Villors");
-		this.chocolats_cible_noms.add("C_MQ_BE_Villors");
-		this.chocolats_cible_noms.add("C_BQ_Villors");
+	
 
 		//this.chocolats.add(c1);
 		//this.stock.ajoutQte(c1, 1000);
@@ -69,23 +62,24 @@ public class Distributeur3Acteur implements IActeur{
 	}
 	
 	public void initialiser() {
+		// william désormais on n'utilise plus une liste de String avec les chocolats qui nous intéressent, on sélectionne seulement à la gamme
+		
 		List<ChocolatDeMarque> chocolats_filiere = new LinkedList<ChocolatDeMarque>();
 		chocolats_filiere = Filiere.LA_FILIERE.getChocolatsProduits();
 		for (int i=0; i<chocolats_filiere.size(); i++) {
-
-			if(chocolats_cible_noms.contains((chocolats_filiere.get(i)).toString())){
+			
+			if(chocolats_filiere.get(i).getGamme() == Gamme.HQ) {
 				chocolats.add(chocolats_filiere.get(i));
 				stock.QteStock.put(chocolats_filiere.get(i),0.0);
 			}
+			else if (chocolats_filiere.get(i).getGamme() == Gamme.MQ) {
+				chocolats.add(chocolats_filiere.get(i));
+				stock.QteStock.put(chocolats_filiere.get(i),0.0);
+			}
+		
 		}
 		System.out.println(chocolats);
-	//	for (int i = 0; i< this.chocolats.size(); i++) {
-		//	this.stock.ajoutQte(chocolats.get(i), 100000000);
-		//	this.prix_tonne_vente.put(chocolats.get(i), 10000.0);
-		//}
-		
-		
-		
+	
 		
 	}
 	
@@ -150,6 +144,7 @@ public class Distributeur3Acteur implements IActeur{
 		// (prendre en compte la rentabilité, le positionnement des autres marques)
 
 	}
+	/*
 	public void repartition_tete_gondole() {
 		HashMap<ChocolatDeMarque, Double> repartition = new HashMap<ChocolatDeMarque, Double>();
 		repartition.put((get_chocolat_with_name("C_HQ_BE_Choc")),1.0);
@@ -161,13 +156,13 @@ public class Distributeur3Acteur implements IActeur{
 	
 	public ChocolatDeMarque get_chocolat_with_name(String name) {
 		for(int i =0; i< chocolats.size();i++) {
-			if( (chocolats.get(i)).toString() == name) {
+			if( (chocolats.get(i)).toString().equals(name)) {
 				return chocolats.get(i);
 			}
 		}
 		return null;
 	}
-	
+	*/
 
 
 	public Color getColor() {// NE PAS MODIFIER
