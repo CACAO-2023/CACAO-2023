acc63a7cc2286a76182545336a62d49d82f16adc
natha <natha@172.16.124.118>
2023-05-03 11:41:01 +0200

    Merge branch 'main' of https://github.com/NathanSlbg/NathanSlbg into main

--- src/abstraction/eq6Transformateur3/Transformateur3Vente.java
+++ src/abstraction/eq6Transformateur3/Transformateur3Vente.java
@@ -67,20 +67,20 @@ public class Transformateur3Vente extends Transformateur3Stocks  implements IVen
 	public double propositionPrix(ExemplaireContratCadre contrat) {
 		if (contrat.getProduit() instanceof ChocolatDeMarque) {
 			if (((ChocolatDeMarque)contrat.getProduit()).getMarque().equals("eco+ choco")) {
-				super.journalVentes.ajouter("Proposition de prix: "+2+" pour :"+((ChocolatDeMarque)contrat.getProduit()).getMarque());
-				return 2;
+				super.journalVentes.ajouter("Proposition de prix: "+2000+" pour :"+((ChocolatDeMarque)contrat.getProduit()).getMarque());
+				return 2000;
 			}
 			if (((ChocolatDeMarque)contrat.getProduit()).getMarque().equals("chokchoco")) {
-				super.journalVentes.ajouter("Proposition de prix: "+2+" pour :"+((ChocolatDeMarque)contrat.getProduit()).getMarque());
-				return 2;
+				super.journalVentes.ajouter("Proposition de prix: "+2100+" pour :"+((ChocolatDeMarque)contrat.getProduit()).getMarque());
+				return 2100;
 			}
 			if (((ChocolatDeMarque)contrat.getProduit()).getMarque().equals("chokchoco bio")) {
-				super.journalVentes.ajouter("Proposition de prix: "+2+" pour :"+((ChocolatDeMarque)contrat.getProduit()).getMarque());
-				return 2;
+				super.journalVentes.ajouter("Proposition de prix: "+2300+" pour :"+((ChocolatDeMarque)contrat.getProduit()).getMarque());
+				return 2300;
 			}
 			if (((ChocolatDeMarque)contrat.getProduit()).getMarque().equals("Choc")) {
-				super.journalVentes.ajouter("Proposition de prix: "+2+" pour :"+((ChocolatDeMarque)contrat.getProduit()).getMarque());
-				return 2;
+				super.journalVentes.ajouter("Proposition de prix: "+2500+" pour :"+((ChocolatDeMarque)contrat.getProduit()).getMarque());
+				return 2500;
 			}
 		}
 		super.journalVentes.ajouter("Proposition de prix: "+0+" pour :"+((ChocolatDeMarque)contrat.getProduit()).getMarque());
