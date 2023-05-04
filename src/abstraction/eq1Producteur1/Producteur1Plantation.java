a2a87fb577adcaa8e69972c6865d49180f061374
eloua <eloua@172.16.127.131>
2023-05-04 10:32:14 +0200

    k

--- src/abstraction/eq1Producteur1/Producteur1Plantation.java
+++ src/abstraction/eq1Producteur1/Producteur1Plantation.java
@@ -14,7 +14,7 @@ public class Producteur1Plantation extends Producteur1Acteur {
 	private Lot stockBasPasSec;
 	private Lot stockMoyPasSec;
 
-//===== 4 premières methodes : elouan =====
+	//===== 4 premières methodes : elouan =====
 
 	public champ getChampBas() {
 		return this.champBas;
@@ -34,7 +34,7 @@ public class Producteur1Plantation extends Producteur1Acteur {
 	public Lot getVraiStockB() {
 		return this.stockFeveBas;
 	}
-	
+
 	public void initialiser () { // elouan
 		//initialisation d'un faible nombre de feves qui sont en sechage
 		super.initialiser();
@@ -44,7 +44,7 @@ public class Producteur1Plantation extends Producteur1Acteur {
 		this.stockMoyPasSec.ajouter(0,10);
 	}
 
-	
+
 	public void next() {
 		//===== début Elouan =====
 		super.next();
@@ -64,25 +64,25 @@ public class Producteur1Plantation extends Producteur1Acteur {
 					Filiere.LA_FILIERE.getBanque().virer(Filiere.LA_FILIERE.getActeur("EQ1"), cryptogramme, Filiere.LA_FILIERE.getActeur("Banque"), this.coutreplantation.getValeur()*q);
 					this.journal_champs.ajouter("Un champ de "+q+" hectares a été planté");
 				}
-					else if ((step-i)%12==0 && step-i>0) 
+				else if ((step-i)%12==0 && step-i>0) 
 					// ===== elouan et début gab =====
-					{
-						double nb_tonnes = q*0.56 ; //ajouter facteur random
-						double random = ThreadLocalRandom.current().nextDouble(0.9, 1.1);
-						nb_tonnes = nb_tonnes * random ;
-						lot_moy.ajouter(step, nb_tonnes); //recolte
-				
-						if (quantiteFeveM.containsKey(step)) {
-							quantiteFeveM.replace(step, quantiteFeveM.get(step)+nb_tonnes);
-						} else {
-							quantiteFeveM.put(step, nb_tonnes);
-						}
-				//ajouter lot moyen et cout replantation 
-						}
+				{
+					double nb_tonnes = q*0.56 ; //ajouter facteur random
+					double random = ThreadLocalRandom.current().nextDouble(0.9, 1.1);
+					nb_tonnes = nb_tonnes * random ;
+					lot_moy.ajouter(step, nb_tonnes); //recolte
+
+					if (quantiteFeveM.containsKey(step)) {
+						quantiteFeveM.replace(step, quantiteFeveM.get(step)+nb_tonnes);
+					} else {
+						quantiteFeveM.put(step, nb_tonnes);
+					}
+					//ajouter lot moyen et cout replantation 
 				}
-		for (Integer i : aretirerM.keySet()) {
-			cm.supprimer(i);
-			cm.ajouter(i, aretirerM.get(i));}
+			}
+			for (Integer i : aretirerM.keySet()) {
+				cm.supprimer(i);
+				cm.ajouter(i, aretirerM.get(i));}
 		}
 		this.journal_champs.ajouter(this.champMoy.toString());
 		this.journal_champs.ajouter("Cela fait en tout "+this.champMoy.getNbHectare()+" hectares");
@@ -112,25 +112,25 @@ public class Producteur1Plantation extends Producteur1Acteur {
 					Filiere.LA_FILIERE.getBanque().virer(Filiere.LA_FILIERE.getActeur("EQ1"), cryptogramme, Filiere.LA_FILIERE.getActeur("Banque"), q*this.coutreplantation.getValeur());
 					this.journal_champs.ajouter("Un champ de "+q+" hectares a été planté");
 				}
-					else if ((step-i)%10==0 && step-i>0) 
-						// ===== elouan et début gab =====
-					{
-						double nb_tonnes = q*0.56 ; //ajouter facteur random
-						double random = ThreadLocalRandom.current().nextDouble(0.9, 1.15);
-						nb_tonnes = nb_tonnes * random ;
-						stockFeveBas.ajouter(step, nb_tonnes); //recolte
-				
-						if (quantiteFeveB.containsKey(step)) {
-							quantiteFeveB.replace(step, quantiteFeveB.get(step)+nb_tonnes);
-						} else {
-							quantiteFeveB.put(step, nb_tonnes);
-						}
+				else if ((step-i)%10==0 && step-i>0) 
+					// ===== elouan et début gab =====
+				{
+					double nb_tonnes = q*0.56 ; //ajouter facteur random
+					double random = ThreadLocalRandom.current().nextDouble(0.9, 1.15);
+					nb_tonnes = nb_tonnes * random ;
+					stockFeveBas.ajouter(step, nb_tonnes); //recolte
+
+					if (quantiteFeveB.containsKey(step)) {
+						quantiteFeveB.replace(step, quantiteFeveB.get(step)+nb_tonnes);
+					} else {
+						quantiteFeveB.put(step, nb_tonnes);
 					}
 				}
+			}
 			for (Integer i : aretirerB.keySet()) {
 				cb.supprimer(i);
 				cb.ajouter(i, aretirerB.get(i));}
-			}
+		}
 		this.journal_champs.ajouter(this.champBas.toString());
 		this.journal_champs.ajouter("Cela fait en tout "+this.champBas.getNbHectare()+" hectares");
 		//on retire les feves perimes
@@ -147,40 +147,40 @@ public class Producteur1Plantation extends Producteur1Acteur {
 		Double q1 = 0.0;
 		Double q2 = 0.0;
 		if (lot_moy_sec!=null) 
-			{q1=lot_moy_sec.getQuantiteTotale();}
+		{q1=lot_moy_sec.getQuantiteTotale();}
 		if (lot_bas_sec!=null) 
-			{q2=lot_moy_sec.getQuantiteTotale();}
+		{q2=lot_moy_sec.getQuantiteTotale();}
 		Filiere.LA_FILIERE.getBanque().virer(Filiere.LA_FILIERE.getActeur("EQ1"), cryptogramme, Filiere.LA_FILIERE.getActeur("Banque"), (q1+q2)*Filiere.LA_FILIERE.getParametre("cout moyen stockage producteur").getValeur());
 		this.journal_stocks.ajouter("Cout de stockage bas de gamme : "+q2*Filiere.LA_FILIERE.getParametre("cout moyen stockage producteur").getValeur());
 		this.journal_stocks.ajouter("Cout de stockage moyenne gamme : "+q1*Filiere.LA_FILIERE.getParametre("cout moyen stockage producteur").getValeur());
-		}
-		//===== fin elouan et gab =====	
-	
-	
+	}
+	//===== fin elouan et gab =====	
+
+
 	//====== gab : calcul du coût de revient pour une qtté donnée d'un produit 
-	
+
 	//méthode qui renvoie le coût de revient d'un stock de fève avant de le vendre
 	//feve chgt qualité?
 	public double calculCoutRevient(IProduit produit, double quantite, int step_recolte) {
 		double cout_revient ;
-			
+
 		//prend en compte la plantation, la main d'oeuvre, le stockage
-			
+
 		if (produit==Feve.F_BQ) {
 			double nb_hectares_BQ = quantite/(1.025*0.56) ; //moyenne du facteur aléatoire (pas possible de connaître exactement le nombre d'hectares)
 			cout_revient = nb_hectares_BQ*Filiere.LA_FILIERE.getParametre("Cout de replantation").getValeur()/(2080*10) //cout replantation/(durée replantation*nb step pousse)
 					+ nb_hectares_BQ * 30 * 10 //hectare*cout main doeuvre*nb step pousse
 					+ quantite*Filiere.LA_FILIERE.getParametre("cout moyen stockage").getValeur()* (step-step_recolte) ;
 		} else {
-				double nb_hectares_MQ = quantite/(0.95*0.56); //moyenne du facteur aléatoire (pas possible de connaître exactement le nombre d'hectares)
-				cout_revient = nb_hectares_MQ*Filiere.LA_FILIERE.getParametre("Cout de replantation").getValeur()/(2080*12) //cout replantation/(durée replantation*nb step pousse)
-						+ nb_hectares_MQ * 30 * 12 //hectare*cout main doeuvre*nb step pousse
-						+ quantite*Filiere.LA_FILIERE.getParametre("cout moyen stockage").getValeur()* (step-step_recolte) ;
+			double nb_hectares_MQ = quantite/(0.95*0.56); //moyenne du facteur aléatoire (pas possible de connaître exactement le nombre d'hectares)
+			cout_revient = nb_hectares_MQ*Filiere.LA_FILIERE.getParametre("Cout de replantation").getValeur()/(2080*12) //cout replantation/(durée replantation*nb step pousse)
+					+ nb_hectares_MQ * 30 * 12 //hectare*cout main doeuvre*nb step pousse
+					+ quantite*Filiere.LA_FILIERE.getParametre("cout moyen stockage").getValeur()* (step-step_recolte) ;
 		}	
 		return cout_revient ;
-			
-		}
-		
+
+	}
+
 	//méthode qui permet d'obtenir le stock dont on veut calculer le coût de revient
 	public Lot fevesConsiderees(IProduit produit, double quantite) {
 		Lot stockFeve ;
@@ -189,8 +189,8 @@ public class Producteur1Plantation extends Producteur1Acteur {
 		} else {
 			stockFeve=stockFeveMoy ;
 		}
-			
-			
+
+
 		Lot lotVente=new Lot(produit);
 		if (quantite<=0) { 
 			throw new IllegalArgumentException("Appel de retirer une qtté <=0");
@@ -199,7 +199,7 @@ public class Producteur1Plantation extends Producteur1Acteur {
 			lotVente = fevesConsiderees(produit, stockFeve.getQuantiteTotale()) ;
 			// même si tt est pas prêt au step d'après on livre au fur et à mesure donc pas d'incidence sur les coûts de stockage
 			lotVente.ajouter(step, quantite_a_produire) ;
-				
+
 		} else {
 			double reste = quantite;
 			for (Integer i : stockFeve.getQuantites().keySet()) {
@@ -215,30 +215,30 @@ public class Producteur1Plantation extends Producteur1Acteur {
 			}
 		}
 		return lotVente ;
-			
+
 	}
-		
+
 	//méthode qui renvoie le coût de revient de la production d'une certaine quantité pour un produit donné 
 	//(on souhaite vendre les fèves les plus vieilles)
 	public double coutRevientQuantite(IProduit produit, double quantite) {
 		Lot lotVente = fevesConsiderees(produit, quantite) ;
 		double cout_revient_total = 0;
-			
+
 		for (Integer j : lotVente.getQuantites().keySet()) {
 			cout_revient_total += calculCoutRevient(produit, lotVente.getQuantites().get(j), j) ;
 		}
-			
+
 		return cout_revient_total ;
-			
+
 	}
-	
+
 	// calcul du coût de revient de la production d'une certaine qqté pour un produit donné avec une marge de 10% pour la vente
 	public double prixMinAvecMarge(IProduit produit, double quantite) {
 		return coutRevientQuantite(produit, quantite)*1.10 ;
 	}
-	
-	
-	
+
+
+
 	//===== fin gab
-	
+
 }
