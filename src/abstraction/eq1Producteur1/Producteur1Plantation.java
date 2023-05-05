c8ade9a60823005127c3a2ead38c086057276994
eloua <eloua@172.16.127.131>
2023-05-05 09:13:14 +0200

    ghf

--- src/abstraction/eq1Producteur1/Producteur1Plantation.java
+++ src/abstraction/eq1Producteur1/Producteur1Plantation.java
@@ -43,11 +43,60 @@ public class Producteur1Plantation extends Producteur1Acteur {
 		this.stockMoyPasSec = new Lot(Feve.F_MQ);
 		this.stockMoyPasSec.ajouter(0,10);
 	}
-
-
+	
+	//===== Elouan =====
+	//méthode qui s'occupe des catastrophes naturelles qui peuvent apparaitre. elle prendra aussi en compte les grèves
 	public void next() {
-		//===== début Elouan =====
 		super.next();
+		champ champm = this.getChampMoy();
+		champ champb = this.getChampBas();
+		this.journal_evenements.ajouter("===== step : "+step+" =====");
+		//on se limite à une catastrophe par next. c'est le role que va jouer b
+		boolean b = true;
+		double greve = Math.random();
+		if (b&&greve<0.08) { //les greves peuvent apparaitre assez frequement 
+			b = false;
+			this.journal_evenements.ajouter("Notre main d'oeuvre fait grève pendant ce step : aucune récoltes ni séchages, mais nous payons les différents couts etc");
+			recolteencasdegreve();
+		}
+		double incendie = Math.random();
+		if (b&&incendie<0.15) { //en cote d'ivoire, les feux peuvent etre frequents a cause du climat tres sec
+			this.journal_champs.ajouter("ATTENTION : un incendie a eu lieu");
+			b = false;
+			if (incendie<0.07) {
+				this.journal_evenements.ajouter("Incendie moyen : 15% des champs sont détruits");
+				champm.supprimer(champm.getNbHectare()*0.15);
+				champb.supprimer(champb.getNbHectare()*0.15);
+			}else if(incendie<0.02){
+				this.journal_evenements.ajouter("Grand incendie : 15% des champs sont détruits");
+				champm.supprimer(champm.getNbHectare()*0.3);
+				champb.supprimer(champb.getNbHectare()*0.3);
+			}else{
+				this.journal_evenements.ajouter("Petit incendie : 5% des champs sont détruits");
+				champm.supprimer(champm.getNbHectare()*0.05);
+				champb.supprimer(champb.getNbHectare()*0.05);
+			}
+			recolte();
+		}
+		double cyclone = Math.random();
+		if (b&&cyclone<0.04) { //cyclone peu fréquent
+			this.journal_champs.ajouter("ATTENTION : un cyclone est passé par nos champs");
+			double random = ThreadLocalRandom.current().nextDouble(0.0, 0.3);
+			this.journal_evenements.ajouter("Cyclone : "+random*100+"% des champs sont détruits");
+			champm.supprimer(champm.getNbHectare()*random);
+			champb.supprimer(champb.getNbHectare()*random);
+			recolte();
+		}
+		if(b) {
+			recolte();
+		}
+		
+	}
+
+
+	public void recolte() {
+		//===== début Elouan =====
+		//cette methode est notre next V1, mais n'est pas toujours appele (en cas de greve par exemple)
 		this.journal_champs.ajouter("===== step : "+step+" =====");
 		this.journal_champs.ajouter("---> Qualite : Moy");
 		Lot lot_moy = this.getStockMoy();
@@ -125,6 +174,87 @@ public class Producteur1Plantation extends Producteur1Acteur {
 					} else {
 						quantiteFeveB.put(step, nb_tonnes);
 					}
+				}
+			}
+			for (Integer i : aretirerB.keySet()) {
+				cb.supprimer(i);
+				cb.ajouter(i, aretirerB.get(i));}
+		}
+		this.journal_champs.ajouter(this.champBas.toString());
+		this.journal_champs.ajouter("Cela fait en tout "+this.champBas.getNbHectare()+" hectares");
+		//on retire les feves perimes
+		if (fevemoyabas!=null) {
+			quantiteFeveB.put(step-6, fevemoyabas); //on rajoute les feves qui ont baisse de qualite pour qu'elles aient une duree de vie de 3 mois
+		}
+		quantiteFeveB.remove(nb_step_perime);
+		// on s'occupe des feves sechées
+		Double nb_feve_sec2 = quantiteFeveB.get(step-1);
+		if (nb_feve_sec2!=null) {
+			lot_bas_sec.ajouter(step, nb_feve_sec2);}
+		quantiteFeveB.remove(step-1);
+		// cout de stockage
+		Double q1 = 0.0;
+		Double q2 = 0.0;
+		if (lot_moy_sec!=null) 
+		{q1=lot_moy_sec.getQuantiteTotale();}
+		if (lot_bas_sec!=null) 
+		{q2=lot_moy_sec.getQuantiteTotale();}
+		Filiere.LA_FILIERE.getBanque().virer(Filiere.LA_FILIERE.getActeur("EQ1"), cryptogramme, Filiere.LA_FILIERE.getActeur("Banque"), (q1+q2)*Filiere.LA_FILIERE.getParametre("cout moyen stockage producteur").getValeur());
+		this.journal_stocks.ajouter("Cout de stockage bas de gamme : "+q2*Filiere.LA_FILIERE.getParametre("cout moyen stockage producteur").getValeur());
+		this.journal_stocks.ajouter("Cout de stockage moyenne gamme : "+q1*Filiere.LA_FILIERE.getParametre("cout moyen stockage producteur").getValeur());
+	}
+	//===== fin elouan et gab =====	
+	
+	public void recolteencasdegreve() {
+		//===== début Elouan =====
+		//cette methode est notre next V1, appele seulement en cas de grèves
+		this.journal_champs.ajouter("===== step : "+step+" =====");
+		this.journal_champs.ajouter("ATTENTION : grève à ce step");
+		this.journal_champs.ajouter("---> Qualite : Moy");
+		Lot lot_moy = this.getStockMoy();
+		Lot lot_moy_sec = this.getVraiStockM();
+		HashMap<Integer, Double> quantiteFeveM = lot_moy.getQuantites();
+		champ cm = this.getChampMoy();
+		if (cm!=null) {
+			HashMap<Integer,Double> aretirerM = new HashMap<Integer,Double>();
+			for (Integer i : cm.getQuantite().keySet()) {
+				double q = cm.getQuantite().get(i);
+				//main d'oeuvre non payé en cas de grève
+				if (step-i==2080) { //supprime l'hectar quand il produit plus, au bout de 40 ans pour la v1
+					aretirerM.put(i,q); //on les stock dans une liste pour les retirer plus tard, sinon on a une erreur
+					//pas de replantation
+				}
+			}
+			for (Integer i : aretirerM.keySet()) {
+				cm.supprimer(i);
+				cm.ajouter(i, aretirerM.get(i));}
+		}
+		this.journal_champs.ajouter(this.champMoy.toString());
+		this.journal_champs.ajouter("Cela fait en tout "+this.champMoy.getNbHectare()+" hectares");
+		this.journal_champs.ajouter("---> Qualite : Bas");
+		//on retire les feves perimes
+		int nb_step_perime = step-12;
+		Double fevemoyabas = null;
+		if (quantiteFeveM!=null) {
+			fevemoyabas = quantiteFeveM.get(nb_step_perime); //quantite de feve qui baisse de gamme, qu'on va rajouter dans le stock de bas
+			quantiteFeveM.remove(nb_step_perime);}
+		// on s'occupe des feves sechées
+		if (quantiteFeveM.get(step-1)!=null) {
+			Double nb_feve_sec = quantiteFeveM.get(step-1);
+			lot_moy_sec.ajouter(step, nb_feve_sec);
+			quantiteFeveM.remove(step-1);}
+		Lot lot_bas = this.getStockBas();
+		HashMap<Integer, Double> quantiteFeveB = lot_bas.getQuantites();
+		Lot lot_bas_sec = this.getVraiStockB();
+		champ cb = this.getChampBas();
+		if (cb!=null) {
+			HashMap<Integer,Double> aretirerB = new HashMap<Integer,Double>();
+			for (Integer i : cb.getQuantite().keySet()) {
+				double q = cb.getQuantite().get(i);
+				//main d'oeuvre non payée
+				if (step-i%2080==0) { //supprime l'hectar quand il produit plus, au bout de 40 ans pour la v1
+					aretirerB.put(i,q); //on les stock dans une liste pour les retirer plus tard, sinon on a une erreur
+					//pas de replantation
 				}
 			}
 			for (Integer i : aretirerB.keySet()) {
