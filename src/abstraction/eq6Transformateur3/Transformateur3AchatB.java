76bbdb918a2d6daedff5e1f498586586501e13d0
natha <natha@172.16.129.128>
2023-05-24 11:17:12 +0200

    m

--- src/abstraction/eq6Transformateur3/Transformateur3AchatB.java
+++ src/abstraction/eq6Transformateur3/Transformateur3AchatB.java
@@ -61,19 +61,19 @@ public class Transformateur3AchatB extends Transformateur3AchatCC implements IAc
 		return res;}
 		else {
 			if (f.getGamme()==Gamme.BQ) {
-				double quant = Math.min(super.quantBQMax-super.stockFeveBG.getQuantiteTotale()-super.quantiteEnAttente-1000,super.totalStocksChoco.getValeur()-200000);
+				double quant = Math.min(super.quantBQMax-super.stockFeveBG.getQuantiteTotale()-super.quantiteEnAttente-1000,super.totalStocksChoco.getValeur()-100000);
 				res= Math.max(0,quant);
 			}
 			if (f.getGamme()==Gamme.MQ) {
-				double quant = Math.min(super.quantMQMax-super.stockFeveMG.getQuantiteTotale()-super.quantiteEnAttente-1000,super.totalStocksChoco.getValeur()-200000);
+				double quant = Math.min(super.quantMQMax-super.stockFeveMG.getQuantiteTotale()-super.quantiteEnAttente-1000,super.totalStocksChoco.getValeur()-100000);
 				res= Math.max(0,quant);
 			}
 			if (f.getGamme()==Gamme.MQ && f.isBioEquitable()) {
-				double quant = Math.min(super.quantMQLMax-super.stockFeveMGL.getQuantiteTotale()-super.quantiteEnAttente-1000,super.totalStocksChoco.getValeur()-200000);
+				double quant = Math.min(super.quantMQLMax-super.stockFeveMGL.getQuantiteTotale()-super.quantiteEnAttente-1000,super.totalStocksChoco.getValeur()-100000);
 				res= Math.max(0,quant);
 			}
 			if (f.getGamme()==Gamme.HQ) {
-				double quant = Math.min(super.quantHQMax-super.stockFeveHGL.getQuantiteTotale()-super.quantiteEnAttente-1000,super.totalStocksChoco.getValeur()-200000);
+				double quant = Math.min(super.quantHQMax-super.stockFeveHGL.getQuantiteTotale()-super.quantiteEnAttente-1000,super.totalStocksChoco.getValeur()-100000);
 				res= Math.max(0,quant);
 			}
 		return res;
