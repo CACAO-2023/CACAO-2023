81671383d46b07208b9627d4c25e8832c5bf7696
addda <addda@172.16.122.18>
2023-05-24 10:12:21 +0200

    Okok

--- src/abstraction/eq5Transformateur2/Transformateur2AcheteurCC.java
+++ src/abstraction/eq5Transformateur2/Transformateur2AcheteurCC.java
@@ -46,7 +46,6 @@ public class Transformateur2AcheteurCC extends Transformateur2Transfo implements
 	@Override
 	public boolean achete(IProduit produit) {
 		// TODO Auto-generated method stub
-		System.out.println(produit+" "+produit.getType());
 		if ((produit.getType().equals("Feve")
 			&& ((((Feve)produit).getGamme()== Gamme.MQ)&&(!((Feve)produit).isBioEquitable())
 			|| ((((Feve)produit).getGamme()== Gamme.HQ)&&(((Feve)produit).isBioEquitable()))))) {
@@ -205,7 +204,6 @@ public class Transformateur2AcheteurCC extends Transformateur2Transfo implements
 		if (cc != null) {   
 			this.journalAchats.ajouter(COLOR_LLGRAY, Color.BLUE, "Contrat cadre passé avec " + vendeur.getNom() + " pour " + produit + "CC : " + cc);
 			this.ContratsAchat.put(cc,vendeur.getNom());
-			System.out.println(ContratsAchat);
 		} 
 		else {
 			this.journalAchats.ajouter(COLOR_LLGRAY, Color.BLUE, "Echec de la négociation de contrat cadre avec " + vendeur.getNom() + " pour " + produit);
