aa0422434d96ed13a13579861ac7d057a9047521
wlabb <wlabb@172.16.128.24>
2023-05-24 08:27:51 +0200

    salut

--- src/abstraction/eq5Transformateur2/Transformateur2VendeurCC.java
+++ src/abstraction/eq5Transformateur2/Transformateur2VendeurCC.java
@@ -168,24 +168,7 @@ public class Transformateur2VendeurCC extends Transformateur2AcheteurCC implemen
 	super.next();
 	this.getContrat(Chocolat.C_MQ);
 	this.getContrat(Chocolat.C_HQ_BE);
->>>>>>> refs/remotes/origin/main
 
-		//this.journalVentes.ajouter(COLOR_LLGRAY, Color.BLUE, "Tentative de négociation de contrat cadre avec " + acheteur.getNom() + " pour " + produit);
-		ExemplaireContratCadre cc = superviseurVentesCC.demandeVendeur(acheteur, this, produit, new Echeancier(Filiere.LA_FILIERE.getEtape()+1, 10, (SuperviseurVentesContratCadre.QUANTITE_MIN_ECHEANCIER+10.0)/10), cryptogramme,false);
-		if (cc != null) {   
-			this.journalVentes.ajouter(COLOR_LLGRAY, Color.BLUE, "Contrat cadre passé avec " + acheteur.getNom() + " pour " + produit + "CC : " + cc);
-			this.ContratsVendeur.put(cc,  acheteur.getNom());
-		} else {
-			this.journalVentes.ajouter(COLOR_LLGRAY, Color.BLUE, "Echec de la négociation de contrat cadre avec " + acheteur.getNom() + " pour " + produit);
-		}
-		return cc; 
-	}
-
-	//fait par wiem 
-	public void next() {
-		super.next();
-		this.getContrat(Chocolat.C_MQ);
-		this.getContrat(Chocolat.C_HQ_BE);
 
 	}
 
