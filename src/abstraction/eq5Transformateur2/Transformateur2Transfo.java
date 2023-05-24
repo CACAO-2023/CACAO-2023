7884237b1e326591ab15c4986b957196d6c8155a
addda <addda@172.16.122.18>
2023-05-24 10:11:52 +0200

    Merge branch 'main' of https://github.com/CACAO-2023/CACAO-2023.git into main

--- src/abstraction/eq5Transformateur2/Transformateur2Transfo.java
+++ src/abstraction/eq5Transformateur2/Transformateur2Transfo.java
@@ -15,7 +15,46 @@ public class Transformateur2Transfo extends Transformateur2Stocks implements IFa
 	
 	private List<ChocolatDeMarque>chocosProduits; // Liste des chocolats de marque produits 
 	protected HashMap<Feve, HashMap<Chocolat, Double>> pourcentageTransfo; // pour les differentes feves, le chocolat qu'elle peuvent contribuer a produire avec le pourcentage de chocolat associé
+	private double MQStep;
+	private double MQteFeve;
+	private double HQStep;
+	private double HQteFeve;
 	
+	
+
+	private double getMQteFeve() {
+		return MQteFeve;
+	}
+
+	private void setMQteFeve(double mQteFeve) {
+		MQteFeve = mQteFeve;
+	}
+
+
+	private double getMQStep() {
+		return MQStep;
+	}
+
+	private double getHQStep() {
+		return HQStep;
+	}
+
+	private double getHQteFeve() {
+		return HQteFeve;
+	}
+
+	private void setMQStep(double mQStep) {
+		MQStep = mQStep;
+	}
+
+	private void setHQStep(double HQStep) {
+		HQStep = HQStep;
+	}
+
+	private void setHQteFeve(double HQteFeve) {
+		HQteFeve = HQteFeve;
+	}
+
 	/**
 	 * @author FERHOUT Adam
 	 */
@@ -39,6 +78,8 @@ public class Transformateur2Transfo extends Transformateur2Stocks implements IFa
 		super.initialiser();
 		
 		// on référence les differents pourcentages en cacao nécéssaires pour les chocolats produits
+		this.setHQStep(0);
+		this.setMQStep(0);
 		
 		this.pourcentageTransfo = new HashMap<Feve, HashMap<Chocolat, Double>>();
 		this.pourcentageTransfo.put(Feve.F_HQ_BE, new HashMap<Chocolat, Double>());
@@ -52,6 +93,22 @@ public class Transformateur2Transfo extends Transformateur2Stocks implements IFa
 	/**
 	 * @author FERHOUT Adam
 	 */
+	public void ajoutStock(double qtefeve, double proportion_marque, Feve f, Chocolat c, String Marque, int cacao) {
+		this.stockChoco.put(c, this.stockChoco.get(c)+((qtefeve*(1-proportion_marque))*this.pourcentageTransfo.get(f).get(c)));
+		this.journal.ajouter(COLOR_LLGRAY, COLOR_BROWN,"Transformation de "+qtefeve*(1-proportion_marque)+" feves "+Journal.texteSurUneLargeurDe(f.getGamme()+"", 15)+" en "+this.stockChoco.get(c)+((qtefeve*(1-proportion_marque))*this.pourcentageTransfo.get(f).get(c))+" chocolat "+Journal.texteSurUneLargeurDe(c.getGamme()+"", 15));
+		double cout = 0;
+			// On paie les coûts de transformation
+		cout = qtefeve*1500; // le coût de transformation est fixé a 1500€ par tonne de feve.
+		Filiere.LA_FILIERE.getBanque().virer(this, this.cryptogramme, Filiere.LA_FILIERE.getActeur("Banque"), cout);
+		this.journal.ajouter(COLOR_LLGRAY, COLOR_LBLUE,"Paiement de "+cout+" pour la transformation de "+qtefeve+" tonnes de feve en chocolat ");
+			
+		ChocolatDeMarque cm = new ChocolatDeMarque(c, Marque, cacao, 0); // le chocolat ChocoPop est a 70% fait de cacao
+		double scm = this.stockChocoMarque.keySet().contains(cm) ?this.stockChocoMarque.get(cm) : 0.0;	
+		this.journal.ajouter(COLOR_LLGRAY, COLOR_BROWN,"Transformation de "+qtefeve*proportion_marque+" feves "+Journal.texteSurUneLargeurDe(f.getGamme()+"", 15)+" en "+((qtefeve*proportion_marque)*this.pourcentageTransfo.get(f).get(c))+Journal.texteSurUneLargeurDe(cm.getMarque(), 15));
+		this.stockChocoMarque.put(cm, scm+((qtefeve*proportion_marque)*this.pourcentageTransfo.get(f).get(c)));					
+		this.totalStocksChocoMarque.ajouter(this, ((qtefeve*proportion_marque)*this.pourcentageTransfo.get(f).get(c)), this.cryptogramme);
+		this.totalStocksChoco.ajouter(this, ((qtefeve*(1-proportion_marque))*this.pourcentageTransfo.get(f).get(c)), this.cryptogramme);
+	}
 	
 	public void next() {
 		super.next();
@@ -83,38 +140,49 @@ public class Transformateur2Transfo extends Transformateur2Stocks implements IFa
 				int cacao = 0;
 				double cout = 0;
 				
-				if (f.getGamme()==Gamme.MQ) {
-					qtefeve = this.stockFeves.get(f)*0.9 ; //15% des feves moyenne gamme sont transformées en ChocoPop, 25% sans marque
+							
+					// On paie les coûts de transformation
+				System.out.println("Stock qui sera transfo"+this.getHQStep()+"  "+this.getHQteFeve()+"  "+this.getMQStep()+"   "+this.getMQteFeve());
+				System.out.println("Gamme "+f.getGamme());
+				if((this.getMQStep()==1)&&(f.getGamme()==Gamme.MQ)) {
+					this.ajoutStock(this.getMQteFeve(), 0.8, f, c, "ChocoPop", 70);
+					this.setMQStep(0);
+					System.out.println("okkk");
+				}
+				else {
+					this.setMQStep(this.getMQStep()+1);
+				}
+				
+				if((this.getHQStep()==2)&&(c.getGamme()==Gamme.HQ)) {
+					this.ajoutStock(this.getHQteFeve(), 1, f, c, "Maison Doutre", 90);
+					this.setHQStep(0);
+				}
+				else {
+					this.setHQStep(this.getHQStep()+1);
+				}
+				
+				if ((f.getGamme()==Gamme.MQ)&&(this.getMQStep()==0)){
+					qtefeve = this.stockFeves.get(f);
+					this.setMQteFeve(qtefeve); //15% des feves moyenne gamme sont transformées en ChocoPop, 25% sans marque
 					proportion_marque = 0.8;
 					Marque = "ChocoPop";
 					cacao = 70;
+					this.stockFeves.put(f, this.stockFeves.get(f)-qtefeve); // on les retire a nos stocks
+					this.totalStocksFeves.retirer(this, qtefeve, this.cryptogramme); // et aux stocks totaux
+					System.out.println("DEBUT TRANSFO CHOCOPOP");
 				}
 				
-				if((c.getGamme()==Gamme.HQ) && (c.isBioEquitable())) {
-					qtefeve = this.stockFeves.get(f)*0.9 ; //10% des feves haute gamme sont transformées en ChocoPop
+				if(((c.getGamme()==Gamme.HQ) && (c.isBioEquitable()))&&(this.getHQStep()==0)) {
+					qtefeve = this.stockFeves.get(f);
+					this.setHQteFeve(this.stockFeves.get(f)); //10% des feves haute gamme sont transformées en ChocoPop
 					proportion_marque = 1;
 					Marque = "Maison Doutre";
 					cacao = 90;
+					this.stockFeves.put(f, this.stockFeves.get(f)-qtefeve); // on les retire a nos stocks
+					this.totalStocksFeves.retirer(this, qtefeve, this.cryptogramme); // et aux stocks totaux
+					System.out.println("DEBUT TRANSFO DE CHOCOLAT");
 				}
-					
-				this.stockFeves.put(f, this.stockFeves.get(f)-qtefeve); // on les retire a nos stocks
-				this.totalStocksFeves.retirer(this, qtefeve, this.cryptogramme); // et aux stocks totaux
-				
-				this.stockChoco.put(c, this.stockChoco.get(c)+((qtefeve*(1-proportion_marque))*this.pourcentageTransfo.get(f).get(c)));
-				this.journal.ajouter(COLOR_LLGRAY, COLOR_BROWN,"Transformation de "+qtefeve*(1-proportion_marque)+" feves "+Journal.texteSurUneLargeurDe(f.getGamme()+"", 15)+" en "+this.stockChoco.get(c)+((qtefeve*(1-proportion_marque))*this.pourcentageTransfo.get(f).get(c))+" chocolat "+Journal.texteSurUneLargeurDe(c.getGamme()+"", 15));
-		
-				// On paie les coûts de transformation
-				cout = qtefeve*1500; // le coût de transformation est fixé a 1500€ par tonne de feve.
-				Filiere.LA_FILIERE.getBanque().virer(this, this.cryptogramme, Filiere.LA_FILIERE.getActeur("Banque"), cout);
-				this.journal.ajouter(COLOR_LLGRAY, COLOR_LBLUE,"Paiement de "+cout+" pour la transformation de "+qtefeve+" tonnes de feve en chocolat ");
-				
-				ChocolatDeMarque cm = new ChocolatDeMarque(c, Marque, cacao, 0); // le chocolat ChocoPop est a 70% fait de cacao
-				double scm = this.stockChocoMarque.keySet().contains(cm) ?this.stockChocoMarque.get(cm) : 0.0;	
-				this.journal.ajouter(COLOR_LLGRAY, COLOR_BROWN,"Transformation de "+qtefeve*proportion_marque+" feves "+Journal.texteSurUneLargeurDe(f.getGamme()+"", 15)+" en "+((qtefeve*proportion_marque)*this.pourcentageTransfo.get(f).get(c))+Journal.texteSurUneLargeurDe(cm.getMarque(), 15));
-				this.stockChocoMarque.put(cm, scm+((qtefeve*proportion_marque)*this.pourcentageTransfo.get(f).get(c)));					
-				this.totalStocksChocoMarque.ajouter(this, ((qtefeve*proportion_marque)*this.pourcentageTransfo.get(f).get(c)), this.cryptogramme);
-				this.totalStocksChoco.ajouter(this, ((qtefeve*(1-proportion_marque))*this.pourcentageTransfo.get(f).get(c)), this.cryptogramme);
-					
+							
 			}
 		}
 	}
