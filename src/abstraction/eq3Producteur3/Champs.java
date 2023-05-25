865a09381e24ee1b29d8319ef15bf5494ff7c313
Gabriel <Gabriel@172.16.127.64>
2023-05-25 18:42:18 +0200

    Sans System.out.println c'est quand même mieux

--- src/abstraction/eq3Producteur3/Champs.java
+++ src/abstraction/eq3Producteur3/Champs.java
@@ -120,7 +120,6 @@ public class Champs {
 		//Ce taux permet de prendre en compte l'aspect aleatoire d'une recolte
 		for (Integer key : HarvestKeys) {
 			double HarvestRate = this.gaussienne(CurrentStep-key);
-			System.out.println(HarvestRate);
 			quantite +=Field.get(key)*HarvestRate;
 		}
 			
